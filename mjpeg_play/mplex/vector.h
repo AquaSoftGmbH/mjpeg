66e878acdf2ed7b7ec8dd90ba84574cbe5f7a2a2
wackston <>
2000-09-02 08:09:23 +0000

    *** empty log message ***

---
+++ mjpeg_play/mplex/vector.h
@@ -0,0 +1,9 @@
+
+typedef struct _Vector *Vector;
+
+Vector NewVector( size_t recsize );
+void *VectorAppend( Vector v, void *rec );
+void VectorRewind( Vector v );
+void *VectorNext( Vector v );
+
+
