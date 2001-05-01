fce7a01f52b8e0de75040dda63480e1fb579a338
rbultje <>
2001-05-01 22:21:33 +0000

    Some changes to make it work 100% with LVS

--- mjpeg_play/utils/mjpeg_logging.c
+++ mjpeg_play/utils/mjpeg_logging.c
@@ -63,15 +63,19 @@ default_mjpeg_log_handler(log_level_t level, const char message[])
   switch(level) {
   case LOG_ERROR:
     fprintf(stderr, "**ERROR: %s", message);
+    fflush(stdout);
     break;
   case LOG_DEBUG:
     fprintf(stderr, "--DEBUG: %s", message);
+    fflush(stdout);
     break;
   case LOG_WARN:
     fprintf(stderr, "++ WARN: %s", message);
+    fflush(stdout);
     break;
   case LOG_INFO:
     fprintf(stderr, "   INFO: %s", message);
+    fflush(stdout);
     break;
   default:
     assert(0);
