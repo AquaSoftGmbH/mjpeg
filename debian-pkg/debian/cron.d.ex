30305b60019dbf91b57a67613456bb1f79a98f39
wackston <>
2001-05-23 19:42:05 +0000

    Initial Debian package control files plus examples for convenience...

---
+++ debian-pkg/debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the mjpegtools package
+#
+0 4	* * *	root	mjpegtools_maintenance
---
+++ debian-pkg/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(mjpegtools):needs=X11|text|vc|wm section=Apps/see-menu-manual\
+  title="mjpegtools" command="/usr/bin/mjpegtools"
