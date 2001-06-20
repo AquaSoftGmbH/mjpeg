995c1bbb8e8797713019a56db35789ede0fb6ec5
biquard <>
2001-06-20 21:53:49 +0000

    Added the handle_args_dependent prototype

--- mjpeg_play/lavtools/yuvscaler.h
+++ mjpeg_play/lavtools/yuvscaler.h
@@ -1,6 +1,7 @@
 int average_coeff(unsigned int,unsigned int,unsigned int *);
 void print_usage(char *argv[]);
 void handle_args_global (int argc, char *argv[]);
+void handle_args_dependent (int argc, char *argv[]);
 int average(unsigned char *,unsigned char *, unsigned int *, unsigned int *,unsigned int);
 int average_specific(unsigned char *,unsigned char *, unsigned int *, unsigned int *, unsigned int,unsigned int);
 unsigned int pgcd(unsigned int,unsigned int);
