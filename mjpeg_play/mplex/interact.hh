850f75882c1d0561975869f58f82b428c25ee0a4
wackston2 <>
2001-08-26 14:17:54 +0000

    Started a more sensible partitioning of classes into source files.

---
+++ mjpeg_play/mplex/interact.hh
@@ -0,0 +1,94 @@
+
+/*
+ *  interact.hh:  Simple command-line front-end
+ *
+ *  Copyright (C) 2001 Andrew Stevens <andrew.stevens@philips.com>
+ *
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of version 2 of the GNU General Public License
+ *  as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef __INTERACT_H__
+#define __INTERACT_H__
+
+#include <config.h>
+#include <unistd.h>
+#include "mjpeg_types.h"
+
+#include "aunit.hh"
+
+/*************************************************************************
+    Entry points...
+*************************************************************************/
+
+    
+int intro_and_options( int, char **, char**);
+
+void check_files (int argc,
+				  char* argv[],
+				  char**audio_file,
+				  char**video_file
+	);
+bool open_file(const char *name);
+
+
+
+
+
+
+/*************************************************************************
+    Command line options and derived parameters
+*************************************************************************/
+
+
+
+extern int opt_quiet_mode;
+extern int opt_interactive_mode;
+extern int opt_buffer_size;
+extern int opt_data_rate;
+extern int opt_video_offset;
+extern int opt_audio_offset;
+extern int opt_sector_size;
+extern int opt_VBR;
+extern int opt_mpeg;
+extern int opt_mux_format;
+extern int opt_multifile_segment;
+extern int opt_always_system_headers;
+extern int opt_packets_per_pack;
+extern clockticks opt_max_PTS;
+extern int opt_emul_vcdmplex;
+extern bool opt_stills;
+extern bool opt_ignore_underrun;
+extern int verbose;
+
+extern off_t opt_max_segment_size;
+
+/*************************************************************************
+    Program ID
+*************************************************************************/
+ 
+#define MPLEX_VER    "2.0.2"
+#define MPLEX_DATE   "$Date$"
+
+
+#endif  __INTERACT_H__
+
+
+/* 
+ * Local variables:
+ *  c-file-style: "gnu"
+ *  tab-width: 8
+ *  indent-tabs-mode: nil
+ * End:
+ */
---
+++ mjpeg_play/mplex/stillsstream.cc
@@ -0,0 +1,199 @@
+
+/*
+ *  stillsstreams.c: Class for elemenary still video streams
+ *                   Most functionality is inherited from VideoStream
+ *
+ *  Copyright (C) 2001 Andrew Stevens <andrew.stevens@philips.com>
+ *
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of version 2 of the GNU General Public License
+ *  as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+#include <format_codes.h>
+
+#include "stillsstream.hh"
+#include "interact.hh"
+#include "outputstream.hh"
+
+void StillsStream::Init ( const char *video_file )
+{
+	int stream_id;
+	int buffer_size;
+    mjpeg_info( "Scanning Stills stream for access units information.\n" );
+	InitAUbuffer();
+	InputStream::Init( video_file );
+	ScanFirstSeqHeader();
+
+	mjpeg_debug( "SETTING video buffer to %d\n", muxinto.video_buffer_size );
+	switch( opt_mux_format )
+	{
+	case  MPEG_FORMAT_VCD_STILL :
+		if( horizontal_size > 352 )
+		{
+			stream_id = VIDEO_STR_0+2 ;
+			buffer_size = vbv_buffer_size*2048;
+			mjpeg_info( "Stream %02x: high-resolution VCD stills %d KB each\n", 
+						stream_id,
+						buffer_size );
+			if( buffer_size < 46*1024 )
+				mjpeg_error_exit1( "I Can't multiplex high-res stills smaller than normal res stills - sorry!\n");
+
+		}
+		else
+		{
+			stream_id = VIDEO_STR_0+1 ;
+			buffer_size = 46*1024;
+			mjpeg_info( "Stream %02x: normal VCD stills\n", stream_id );
+		}
+		break;
+	case MPEG_FORMAT_SVCD_STILL :
+		if( horizontal_size > 480 )
+		{
+			stream_id = VIDEO_STR_0+1;
+			buffer_size = 230*1024;
+			mjpeg_info( "Stream %d: high-resolution SVCD stills.\n", 
+						stream_id );
+		}
+		else
+		{
+			stream_id = VIDEO_STR_0+1 ;
+			buffer_size = 230*1024;
+			mjpeg_info( "Stream %d: normal-resolution SVCD stills.\n", stream_id );
+		}
+		break;
+	defaut:
+		mjpeg_error_exit1( "Only SVCD and VCD Still currently supported\n");
+	}
+
+
+	MuxStream::Init( stream_id,
+					 1,  // Buffer scale
+					 buffer_size,
+					 0,  // Zero stuffing
+					 muxinto.buffers_in_video,
+					 muxinto.always_buffers_in_video);
+	
+	/* Skip to the end of the 1st AU (*2nd* Picture start!)
+	*/
+	AU_hdr = SEQUENCE_HEADER;
+	AU_pict_data = 0;
+	AU_start = 0LL;
+
+    OutputSeqhdrInfo();
+
+}
+
+
+
+
+/*
+ * Compute DTS / PTS for a VCD/SVCD Stills sequence
+ * TODO: Very crude. Simply assumes each still stays for the specified
+ * frame interval and that enough run-in delay is present for the first
+ * frame to be loaded.
+ *
+ */
+
+void StillsStream::NextDTSPTS( clockticks &DTS, clockticks &PTS )
+{
+	clockticks interval = static_cast<clockticks>
+		(intervals->NextFrameInterval() * CLOCKS / frame_rate);
+	clockticks time_for_xfer;
+	muxinto.ByteposTimecode( BufferSize(), time_for_xfer );
+		
+	DTS = current_PTS + time_for_xfer;	// This frame decoded just after
+	                                    // Predecessor completed.
+	PTS = current_PTS + time_for_xfer + interval;
+	current_PTS = PTS;
+	current_DTS = DTS;
+}
+
+/*
+ * VCD mixed stills segment items have the constraint that both stills
+ * streams must end together.  To do this each stream has to know
+ * about its "sibling".
+ *
+ */
+
+void VCDStillsStream::SetSibling( VCDStillsStream *_sibling )
+{
+	assert( _sibling != 0 );
+	sibling = _sibling;
+	if( sibling->stream_id == stream_id )
+	{
+		mjpeg_error_exit1("VCD mixed stills stream cannot contain two streams of the same type!\n");
+	}
+
+}
+
+/*
+ * Check if we've reached the last sector of the last AU.  Note: that
+ * we know no PTS/DTS time-stamps will be needed because no new AU
+ * will appear in the last sector.  WARNING: We assume a still won't
+ * fit into a single secotr.
+ *
+ */
+
+bool VCDStillsStream::LastSectorLastAU()
+{
+	return ( Lookahead() == 0 &&
+			 au_unsent <= muxinto.PacketPayload( *this,
+												 buffers_in_header, 
+												 false, false )
+		);
+}
+
+
+/*
+ * The requirement that VCD mixed stills segment items constituent streams
+ * end together means we can't mux the last sector of the last AU of
+ * such streams until its sibling has already completed muxing or is
+ * also ready to mux the last sector of its last AU.
+ *
+ * NOTE: Will not work right if sector_align_iframe_AUs not set as this
+ * will allow multiple AU's in  a sector.
+ *
+ */
+
+
+bool VCDStillsStream::MuxPossible()
+{
+	if (RunOutComplete() ||	bufmodel.space() <= au_unsent)
+	{
+		return false;
+	}
+	
+	if( LastSectorLastAU() )
+	{
+		if( sibling == 0 ||
+			sibling->MuxCompleted() || sibling->LastSectorLastAU() )
+			return true;
+		else
+			return false;
+	}
+	else
+		return true;
+}
+
+
+
+
+/* 
+ * Local variables:
+ *  c-file-style: "stroustrup"
+ *  tab-width: 4
+ *  indent-tabs-mode: nil
+ * End:
+ */
