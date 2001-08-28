c090d7086baae98524faa5c3916b044665e17e69
kwmt <>
2001-08-28 17:01:21 +0000

    use config.h

--- mjpeg_play/yuvfilters/yuvfilters.h
+++ mjpeg_play/yuvfilters/yuvfilters.h
@@ -1,15 +1,46 @@
 /* -*- mode:C -*- */
+/*
+ *  Copyright (C) 2001 Kawamata/Hitoshi <hitoshi.kawamata@nifty.ne.jp>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
-#ifndef YUVFILTERS_H
-#define YUVFILTERS_H
+#ifndef __YUVFILTERS_H__
+#define __YUVFILTERS_H__
 
 #include <stddef.h>
+#ifdef MJPEGTOOLS
+# include <string.h>
+# include <errno.h>
+# include <mjpeg_logging.h>
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define WERROR(MSG) write(2, MSG, sizeof MSG - 1)
+#ifdef MJPEGTOOLS
+# define perror(MSG) mjpeg_error("%s: %s\n", MSG, strerror(errno))
+# define WERROR(MSG) mjpeg_error(MSG)
+# define WERRORL(MSG) mjpeg_error(MSG)
+# define WWARN(MSG) mjpeg_warn(MSG)
+#else
+# define WERROR(MSG) write(2, MSG, sizeof MSG - 1)
+# define WERRORL(MSG) write(2, MSG, strlen(MSG))
+# define WWARN(MSG) WERROR("warning: " MSG)
+#endif
 
 typedef struct {
   char id[sizeof "FRAME\n" - 1];
--- mjpeg_play/yuvfilters/yuvycsnoise.c
+++ mjpeg_play/yuvfilters/yuvycsnoise.c
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (C) 2001 Kawamata/Hitoshi <hitoshi.kawamata@nifty.ne.jp>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <config.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -102,7 +121,7 @@ do_init(int argc, char **argv, const YfTaskCore_t *h0)
     return NULL;
   }
   if (h0->height != 480 || h0->fpscode != 4)
-    WERROR("warning: input doesn't seem NTSC full height / full motion video\n");
+    WWARN("input doesn't seem NTSC full height / full motion video\n");
   h = (YfTask_t *)
     YfAllocateTask(&yuvycsnoise,
 		   (sizeof *h + DATABYTES(h0->width, h0->height) + /* frame */
