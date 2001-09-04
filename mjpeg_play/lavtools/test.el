ad57635d4fde1d0a6b6f076f6e4d759a3745b29c
kwmt <>
2001-09-04 21:54:49 +0000

    remove unused files

--- mjpeg_play/lavtools/test.el
+++
@@ -1,5 +0,0 @@
-LAV Edit List
-PAL
-1
-/mnt/pistol/c/avi/evhor5.avi
-0 3527 7804
--- mjpeg_play/yuvscaler/yuvscaler.h
+++
@@ -1,7 +0,0 @@
-int average_coeff(unsigned int,unsigned int,unsigned int *);
-void print_usage(char *argv[]);
-void handle_args_global (int argc, char *argv[]);
-int average_along_width(unsigned char *,unsigned int *,unsigned char *,int);
-int average_along_height(unsigned char *,unsigned int *,unsigned char *,int);
-int average(unsigned char *,unsigned char *, unsigned int *, unsigned int *, int);
-unsigned int pgcd(unsigned int,unsigned int);
