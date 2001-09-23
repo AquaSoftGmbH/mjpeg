8b2ccd87c30e4c4e1eac96cb9ed205d8f69aea64
biquard <>
2001-09-23 17:11:09 +0000

    Now using the new YUV4MPEG header

--- mjpeg_play/lavtools/yuvscaler.h
+++ mjpeg_play/lavtools/yuvscaler.h
@@ -3,12 +3,13 @@ void print_usage(char *argv[]);
 void handle_args_global (int argc, char *argv[]);
 void handle_args_dependent (int argc, char *argv[]);
 int average(unsigned char *,unsigned char *, unsigned int *, unsigned int *,unsigned int);
-int average_specific(unsigned char *,unsigned char *, unsigned int *, unsigned int *, unsigned int,unsigned int);
+int average_specific(unsigned char *,unsigned char *, unsigned int *, unsigned int *, unsigned int);
 unsigned int pgcd(unsigned int,unsigned int);
-int cubic_scale (uint8_t *, uint8_t *, unsigned int *,float *,unsigned int *, float *,unsigned long int *,
-		 unsigned long int *, unsigned int);
-long int cubic_spline(float);
+int cubic_scale (uint8_t *, uint8_t *, unsigned int *,float *,unsigned int *, float *,int16_t *,int16_t *, unsigned int);
+int16_t cubic_spline(float);
 int padding(uint8_t *,uint8_t *,unsigned int); 
 int padding_interlaced (uint8_t *, uint8_t *, uint8_t *, unsigned int);
-int cubic_scale_interlaced (uint8_t *, uint8_t *, uint8_t *, unsigned int * ,float *,unsigned int *, float *,
-			    unsigned long int *,unsigned long int *, unsigned int);
+int cubic_scale_interlaced (uint8_t *, uint8_t *, uint8_t *, unsigned int * ,float *,unsigned int *,
+			    float *, int16_t *, int16_t *, unsigned int);
+int  my_y4m_read_frame(int, y4m_frame_info_t *, unsigned long int, char *, int);
+int line_switch (uint8_t * input, uint8_t *line);
