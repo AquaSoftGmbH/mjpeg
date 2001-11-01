2668b503aff875cb965aaf0a2cc25deab18f9c03
biquard <>
2001-11-01 00:26:16 +0000

    Adapted to CVS version 1.24 of yuvscaler.c

--- mjpeg_play/lavtools/yuvscaler.h
+++ mjpeg_play/lavtools/yuvscaler.h
@@ -6,7 +6,7 @@ int average(unsigned char *,unsigned char *, unsigned int *, unsigned int *,unsi
 int average_specific(unsigned char *,unsigned char *, unsigned int *, unsigned int *, unsigned int);
 unsigned int pgcd(unsigned int,unsigned int);
 int cubic_scale (uint8_t *, uint8_t *, unsigned int *,float *,unsigned int *, float *,int16_t *,int16_t *, unsigned int);
-int16_t cubic_spline(float);
+int16_t cubic_spline(float,unsigned int);
 int padding(uint8_t *,uint8_t *,unsigned int); 
 int padding_interlaced (uint8_t *, uint8_t *, uint8_t *, unsigned int);
 int cubic_scale_interlaced (uint8_t *, uint8_t *, uint8_t *, unsigned int * ,float *,unsigned int *,
