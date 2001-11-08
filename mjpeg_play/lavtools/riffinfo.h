7555c23d23011d20d96cd777f11220aefc872626
rbultje <>
2001-11-08 17:52:29 +0000

    Forgot this one

---
+++ mjpeg_play/lavtools/riffinfo.h
@@ -0,0 +1,36 @@
+#ifndef RIFFINFO_YV12
+#define RIFFINFO_YV12 fccYV12
+#endif
+#ifndef RIFFINFO_DIV3
+#define RIFFINFO_DIV3 fccDIV3
+#endif
+#ifndef RIFFINFO_DIV4
+#define RIFFINFO_DIV4 fccDIV4
+#endif
+#ifndef RIFFINFO_DIV4
+#define RIFFINFO_DIV4 fccDIV4
+#endif
+#ifndef RIFFINFO_DIV5
+#define RIFFINFO_DIV5 fccDIV5
+#endif
+#ifndef RIFFINFO_DIV6
+#define RIFFINFO_DIV6 fccDIV6
+#endif
+#ifndef RIFFINFO_I420
+#define RIFFINFO_I420 fccI420
+#endif
+#ifndef RIFFINFO_YVYU
+#define RIFFINFO_YVYU fccYVYU
+#endif
+#ifndef RIFFINFO_YUY2
+#define RIFFINFO_YUY2 fccYUY2
+#endif
+#ifndef RIFFINFO_UYVY
+#define RIFFINFO_UYVY fccUYVY
+#endif
+#ifndef RIFFINFO_MJPG
+#define RIFFINFO_MJPG fccMJPG
+#endif
+#ifndef RIFFINFO_IV50
+#define RIFFINFO_IV50 fccIV50
+#endif
