e3c66b1fa411e4a47f8e27f338b047ef1156ceae
waldviertler <>
2001-11-11 10:32:52 +0000

    Uploaded a fixed version of yuvmedianfilter compatible with the new format.
    Changes made by Steven M. Schultz <sms@2BSD.COM>

--- mjpeg_play/lavtools/yuvmedianfilter.c
+++ mjpeg_play/lavtools/yuvmedianfilter.c
@@ -20,16 +20,13 @@
  *    This filter look around the current point for a radus and averages
  *    this values that fall inside a threshold.
  */
+#include <config.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "yuv4mpeg.h"
 #include "mjpeg_logging.h"
-
-#define	CHROMA420 1
-#define	CHROMA422 2
-#define	CHROMA444 3
 
 #define	PIXEL_AVG \
 	diff = reference - *++pixel; \
@@ -42,11 +39,7 @@ int verbose = 1;
 unsigned char	*input_frame[3];
 unsigned char	*output_frame[3];
 
-void	read_yuv(int fd, int *horizontal, int *vertical, int *frame_rate_code);
-int	read_frame(int fd, int chroma, int width, int height, unsigned char *frame[]);
-int	write_frame(int fd, int chroma, int width, int height, unsigned char *frame[]);
-int	piperead(int fd, char *buf, int len);
-void	filter(int width, int height, int chroma, unsigned char *input[], unsigned char *output[]);
+void	filter(int width, int height, unsigned char *input[], unsigned char *output[]);
 void	filter_buffer(int width, int height, int radus, int threshold, unsigned char *input, unsigned char *output);
 
 int	threshold_luma = 2;
@@ -73,21 +66,16 @@ main(int argc, char *argv[])
 {
 	int	i;
 	int	avg;
-	int	input_fd;
-	int	output_fd;
+	int	input_fd = 0;
+	int	output_fd = 1;
 	int	horz;
 	int	vert;
-	int	frame_rate;
-	int	eof;
 	int	c;
 	int	frame_count;
-	char	line[256];
-
-	input_fd = 0;
-	output_fd = 1;
+	y4m_stream_info_t istream, ostream;
+	y4m_frame_info_t iframe;
 
 	while((c = getopt(argc, argv, "r:t:v:")) != EOF) {
-
 		switch(c) {
 			case 'r':
 				radus_luma = radus_chroma = atoi(optarg);
@@ -111,181 +99,51 @@ main(int argc, char *argv[])
 
    (void)mjpeg_default_handler_verbosity(verbose);
 
-	read_yuv(input_fd, &horz, &vert, &frame_rate);
+	y4m_init_stream_info(&istream);
+	y4m_init_frame_info(&iframe);
 
-	mjpeg_debug( "width=%d height=%d rate=%d\n", horz, vert, frame_rate);
+	i = y4m_read_stream_header(input_fd, &istream);
+	if (i != Y4M_OK)
+		mjpeg_error_exit1("Input stream error: %s\n", y4m_strerr(i));
+
+	horz = istream.width;
+	vert = istream.height;
+	mjpeg_debug( "width=%d height=%d\n", horz, vert);
+
+	y4m_copy_stream_info(&ostream, &istream);
+
 	input_frame[0] = alloca(horz * vert);
-	input_frame[1] = alloca(horz * vert);
-	input_frame[2] = alloca(horz * vert);
+	input_frame[1] = alloca((horz / 2) * (vert / 2));
+	input_frame[2] = alloca((horz / 2) * (vert / 2));
 
 	output_frame[0] = alloca(horz * vert);
-	output_frame[1] = alloca(horz * vert);
-	output_frame[2] = alloca(horz * vert);
+	output_frame[1] = alloca((horz / 2) * (vert / 2));
+	output_frame[2] = alloca((horz / 2) * (vert / 2));
 
+
+	y4m_write_stream_header(output_fd, &ostream);
 
 	frame_count = 0;
-
-	sprintf(line, "YUV4MPEG %d %d %d\n", horz, vert, frame_rate);
-	write(output_fd, line, strlen(line));
-
-	for(;;) {
-		eof = read_frame(input_fd, CHROMA420, horz, vert, input_frame);
-		if (eof) {
-			break;
-		}
+	while (y4m_read_frame(input_fd, &istream, &iframe, input_frame) == Y4M_OK)
+		{ 
 		frame_count++;
-		filter(horz, vert, CHROMA420, input_frame, output_frame);
+		filter(horz, vert, input_frame, output_frame);
+		y4m_write_frame(output_fd, &ostream, &iframe, output_frame);
+		}
 
-		write_frame(output_fd, CHROMA420, horz, vert, output_frame);
-	}
-
-	for(avg=0, i=0; i < 64; i++)
+	for (avg=0, i=0; i < 64; i++)
 		avg += avg_replace[i];
 
-	mjpeg_info( "\nframes=%d avg=%d replaced=%d\n", avg, chg_replace, ovr_replace);
-	for(i=0; i < 64; i++) {
+	mjpeg_info("\nframes=%d avg=%d replaced=%d\n", avg, chg_replace, ovr_replace);
+	for (i=0; i < 64; i++) {
 		mjpeg_debug( "%02d: %6.2f\n", i,
-					 (((double)avg_replace[i]) * 100.0)/(double)(avg));
+			(((double)avg_replace[i]) * 100.0)/(double)(avg));
 	}
-
 	exit(0);
 }
 
-
 void
-read_yuv(int fd, int *horizontal, int *vertical, int *frame_rate_code)
-{
-	int	n;
-	char	buffer[256];
-
-	for(n=0; n<255; n++) {
-		if(!read(fd, &buffer[n], 1)) {
-			mjpeg_error_exit1("Error reading header\n");
-			exit(1);
-		}
-
-		if(buffer[n]=='\n') 
-			break;
-	}
-	if(buffer[n] != '\n') {
-		mjpeg_error_exit1("Didn't get linefeed in first %d characters of data\n",
-				255);
-		exit(1);
-	}
-
-	buffer[n] = 0; /* Replace linefeed by end of string */
-
-	if(strncmp(buffer,"YUV4MPEG",8))
-	{
-		mjpeg_error_exit1("Input starts not with \"YUV4MPEG\"\n");
-		mjpeg_error_exit1("This is not a valid input for me\n");
-		exit(1);
-	}
-
-
-	sscanf(&buffer[8], "%d %d %d", horizontal, vertical, frame_rate_code);
-}
-
-
-int
-read_frame(int fd, int chroma, int width, int height, unsigned char *frame[])
-{
-	char	buffer[256];
-	int	v;
-	int	h;
-	int	i;
-
-	if (piperead(fd, buffer, 6) != 6) {
-		return 1;
-	}
-
-
-	if (memcmp(buffer, "FRAME\n", 6)) {
-		mjpeg_error_exit1( "Start of new frame missing (%.6s\n", buffer);
-	}
-
-	v = height;
-	h = width;
-	for(i=0; i < v; i++) {
-		if (piperead(fd, frame[0] + i * width, h) != h) {
-			mjpeg_error_exit1( "EOF while reading luma data\n");
-		}
-	}
-	
-
-	v = chroma == CHROMA420 ? height/2 : height;
-	h = chroma == CHROMA420 ? width/2 : width;
-	for(i=0; i < v; i++) {
-		if (piperead(fd, frame[1] + i * h, h) != h) {
-			mjpeg_error_exit1( "EOF while reading chroma u data\n");
-		}
-	}
-	for(i=0; i < v; i++) {
-		if (piperead(fd, frame[2] + i * h, h) != h) {
-			mjpeg_error_exit1( "EOF while reading chroma v data\n");
-		}
-	}
-
-	return 0;
-}
-
-int
-write_frame(int fd, int chroma, int width, int height, unsigned char *frame[])
-{
-	int	v;
-	int	h;
-	int	i;
-
-	if (write(fd, "FRAME\n", 6) != 6) {
-		mjpeg_error_exit1( "Can not write Frame to output\n");
-	}
-
-	v = height;
-	h = width;
-	for(i=0; i < v; i++) {
-		if (write(fd, frame[0] + i * width, h) != h) {
-			mjpeg_error_exit1( "Error writting reading luma data\n");
-		}
-	}
-	
-
-	v = chroma == CHROMA420 ? height/2 : height;
-	h = chroma == CHROMA420 ? width/2 : width;
-	for(i=0; i < v; i++) {
-		if (write(fd, frame[1] + i * h, h) != h) {
-			mjpeg_error_exit1( "EOF while writting chroma u data\n");
-		}
-	}
-	for(i=0; i < v; i++) {
-		if (write(fd, frame[2] + i * h, h) != h) {
-			mjpeg_error_exit1( "EOF while writting chroma v data\n");
-		}
-	}
-
-	return 0;
-}
-
-int
-piperead(int fd, char *buf, int len)
-{
-	int	n;
-	int	r;
-
-	r = 0;
-	n = 0;
-
-	do {
-		n = read(fd, buf + r, len-r);
-		if (n == 0)
-			return r;
-		r += n;
-	} while(r < len);
-
-	return r;
-}
-
-void
-filter(int width, int height, int chroma, unsigned char *input[], unsigned char *output[])
+filter(int width, int height, unsigned char *input[], unsigned char *output[])
 {
 	filter_buffer(width, height, radus_luma, threshold_luma, input[0], output[0]);
 	filter_buffer(width/2, height/2, radus_chroma, threshold_chroma, input[1], output[1]);
