3805028c5760027c07fe7667ba0d1b0a1dea8459
rbultje <>
2001-12-25 10:53:18 +0000

    Updated for new GNU tools with libtoolize

--- studio/autogen.sh
+++ studio/autogen.sh
@@ -3,12 +3,17 @@ touch config.sub
 chmod a+x config.sub
 touch config.guess
 chmod a+x config.guess
+echo "Running libtoolize..."
+libtoolize --force --copy
 if test ! -r aclocal.m4; then
   echo "Running aclocal ..."
   aclocal 
 else
   echo "Skiping aclocal, because aclocal.m4 exists"
 fi
+echo "Running autoheader..."
 autoheader
+echo "Running autoconf..."
 autoconf
-automake
+echo "Running automake..."
+automake --add-missing --gnu
