c82ef6e6eb3ba45165d51ecb78b8878439c1bda0
rbultje <>
2002-03-24 21:49:42 +0000

    Apply an old patch fixing interlacing issues with the Buz

--- driver-zoran/zoran.c
+++ driver-zoran/zoran.c
@@ -2141,7 +2141,7 @@ static void zr36057_set_jpg(struct zoran *zr, enum zoran_codec_mode mode)
 		reg = ZR36057_FPP_Odd_Even;
 	else
 		reg = 0;
-	if (mode == BUZ_MODE_MOTION_DECOMPRESS && zr->card != LML33 && zr->card != BUZ)
+	if ((mode == BUZ_MODE_MOTION_DECOMPRESS && zr->card != LML33) || zr->card != BUZ)
 		reg ^= ZR36057_FPP_Odd_Even;
 
 	btwrite(reg, ZR36057_FPP);
