a4e2a56086f36fb687795f45f80d24595bddca10
waldviertler <>
2002-10-19 14:48:56 +0000

    Thist is the install script for the mjpeg howto in the info format. And the mjpeg-howto in the info format. After the howto is installed you should be able to read it using: > info mjpeg-howto

---
+++ mjpeg_play/docs/install-info.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# cp *.info* $1/.
+
+install-info --help | grep menuentry >/dev/null
+
+if [ $? -eq 0 ]
+then
+  # Here we install the doku for debian style install-info
+  install-info $1/mjpeg-howto.info $1/dir --menuentry=\
+"* mjpeg-howto: (mjpeg-howto).        How to use the mjpeg tools"
+else
+  # Here we install the doku for gnu style install-info
+  install-info $1/mjpeg-howto.info $1/dir --entry=\
+"* mjpeg-howto: (mjpeg-howto).        How to use the mjpeg tools"
+fi
+
