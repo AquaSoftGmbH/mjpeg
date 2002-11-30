ed77ba036535ff0782b2280dc920e2506d1a3fd8
mikebern <>
2002-11-30 22:27:21 +0000

    Added playback of DV files format dv2 422 and 411

--- mjpeg_play/lavtools/liblavplay.c
+++ mjpeg_play/lavtools/liblavplay.c
@@ -77,6 +77,122 @@
 /*#include "editlist.h" */
 #include "jpegutils.h"
 
+#ifdef	SUPPORT_READ_DV2
+#include <libdv/dv.h>
+
+dv_decoder_t *decoder;
+#ifdef LIBDV_PRE_0_9_5
+gint pitches[3];
+#else
+int pitches[3];
+#endif
+uint8_t *dv_frame[3] = {NULL,NULL,NULL};
+/*
+ * As far as I (maddog) can tell, this is what is going on with libdv-0.9
+ *  and the unpacking routine... 
+ *    o Ft/Fb refer to top/bottom scanlines (interleaved) --- each sample
+ *       is implicitly tagged by 't' or 'b' (samples are not mixed between
+ *       fields)
+ *    o Indices on Cb and Cr samples indicate the Y sample with which
+ *       they are co-sited.
+ *    o '^' indicates which samples are preserved by the unpacking
+ *
+ * libdv packs both NTSC 4:1:1 and PAL 4:2:0 into a common frame format of
+ *  packed 4:2:2 pixels as follows:
+ *
+ *
+ ***** NTSC 4:1:1 *****
+ *
+ *   libdv's 4:2:2-packed representation  (chroma repeated horizontally)
+ *
+ *Ft Y00.Cb00.Y01.Cr00.Y02.Cb00.Y03.Cr00 Y04.Cb04.Y05.Cr04.Y06.Cb04.Y07.Cr04
+ *    ^    ^   ^    ^   ^    ^   ^    ^   ^    ^   ^    ^   ^    ^   ^    ^
+ *Fb Y00.Cb00.Y01.Cr00.Y02.Cb00.Y03.Cr00 Y04.Cb04.Y05.Cr04.Y06.Cb04.Y07.Cr04
+ *    ^    ^   ^    ^   ^    ^   ^    ^   ^    ^   ^    ^   ^    ^   ^    ^
+ *Ft Y10.Cb10.Y11.Cr10.Y12.Cb10.Y13.Cr10 Y14.Cb14.Y15.Cr14.Y16.Cb14.Y17.Cr14
+ *    ^        ^        ^        ^        ^        ^        ^        ^    
+ *Fb Y10.Cb10.Y11.Cr10.Y12.Cb10.Y13.Cr10 Y14.Cb14.Y15.Cr14.Y16.Cb14.Y17.Cr14
+ *    ^        ^        ^        ^        ^        ^        ^        ^    
+ *
+ *    lavtools unpacking into 4:2:0-planar  (note lossiness)
+ *
+ *Ft  Y00.Y01.Y02.Y03.Y04.Y05.Y06.Y07...
+ *Fb  Y00.Y01.Y02.Y03.Y04.Y05.Y06.Y07...
+ *Ft  Y10.Y11.Y12.Y13.Y14.Y15.Y16.Y17...
+ *Fb  Y10.Y11.Y12.Y13.Y14.Y15.Y16.Y17...
+ *
+ *Ft  Cb00.Cb00.Cb04.Cb04...    Cb00,Cb04... are doubled
+ *Fb  Cb00.Cb00.Cb04.Cb04...    Cb10,Cb14... are ignored
+ *
+ *Ft  Cr00.Cr00.Cr04.Cr04...
+ *Fb  Cr00.Cr00.Cr04.Cr04...
+ *
+ * 
+ ***** PAL 4:2:0 *****
+ *
+ *   libdv's 4:2:2-packed representation  (chroma repeated vertically)
+ *
+ *Ft Y00.Cb00.Y01.Cr10.Y02.Cb02.Y03.Cr12 Y04.Cb04.Y05.Cr14.Y06.Cb06.Y07.Cr16
+ *    ^    ^   ^    ^   ^    ^   ^    ^   ^    ^   ^    ^   ^    ^   ^    ^
+ *Fb Y00.Cb00.Y01.Cr10.Y02.Cb02.Y03.Cr12 Y04.Cb04.Y05.Cr14.Y06.Cb06.Y07.Cr16
+ *    ^    ^   ^    ^   ^    ^   ^    ^   ^    ^   ^    ^   ^    ^   ^    ^
+ *Ft Y10.Cb00.Y11.Cr10.Y12.Cb02.Y13.Cr12 Y14.Cb04.Y15.Cr14.Y16.Cb06.Y17.Cr16
+ *    ^        ^        ^        ^        ^        ^        ^        ^    
+ *Fb Y10.Cb00.Y11.Cr10.Y12.Cb02.Y13.Cr12 Y14.Cb04.Y15.Cr14.Y16.Cb06.Y17.Cr16
+ *    ^        ^        ^        ^        ^        ^        ^        ^    
+ *
+ *    lavtools unpacking into 4:2:0-planar
+ *
+ *Ft  Y00.Y01.Y02.Y03.Y04.Y05.Y06.Y07...
+ *Fb  Y00.Y01.Y02.Y03.Y04.Y05.Y06.Y07...
+ *Ft  Y10.Y11.Y12.Y13.Y14.Y15.Y16.Y17...
+ *Fb  Y10.Y11.Y12.Y13.Y14.Y15.Y16.Y17...
+ *
+ *Ft  Cb00.Cb02.Cb04.Cb06...
+ *Fb  Cb00.Cb02.Cb04.Cb06...
+ *
+ *Ft  Cr10.Cr12.Cr14.Cr16...
+ *Fb  Cr10.Cr12.Cr14.Cr16...
+ *
+ */
+
+/*
+ * Unpack libdv's 4:2:2-packed into our 4:2:0-planar
+ *
+ */
+void frame_YUV422_to_YUV420P(uint8_t **output, uint8_t *input,
+			     int width, int height)
+{
+    int i, j, w2;
+    uint8_t *y, *cb, *cr;
+
+    w2 = width/2;
+    y = output[0];
+    cb = output[1];
+    cr = output[2];
+
+    for (i=0; i<height; i+=2) {
+	/* process two scanlines (one from each field, interleaved) */
+        for (j=0; j<w2; j++) {
+            /* packed YUV 422 is: Y[i] U[i] Y[i+1] V[i] */
+            *(y++) =  *(input++);
+            *(cb++) = *(input++);
+            *(y++) =  *(input++);
+            *(cr++) = *(input++);
+        }
+	/* process next two scanlines (one from each field, interleaved) */
+	for (j=0; j<w2; j++) {
+	  /* skip every second line for U and V */
+	  *(y++) = *(input++);
+	  input++;
+	  *(y++) = *(input++);
+	  input++;
+	}
+    }
+}
+
+#endif
+
 
 /* On some systems MAP_FAILED seems to be missing */
 #ifndef MAP_FAILED
@@ -527,7 +643,11 @@ static int lavplay_SDL_update(lavplay_t *info, uint8_t *jpeg_buffer,
 			      int buf_len)
 {
    video_playback_setup *settings = (video_playback_setup *)info->settings;
+# ifdef SUPPORT_READ_DV2
+   uint8_t *output[3];
+#endif
    EditList *editlist = info->editlist;
+
 
    if (!lavplay_SDL_lock(info)) return 0;
 
@@ -560,7 +680,40 @@ static int lavplay_SDL_update(lavplay_t *info, uint8_t *jpeg_buffer,
 # endif
 # ifdef SUPPORT_READ_DV2
    case DATAFORMAT_DV2:
-     /* FIXME: not implemented, do default: */
+      dv_parse_header(decoder, jpeg_buffer);
+      switch(decoder->sampling) {
+      /* FIXME: not implemented, do default: */
+      case e_dv_sample_420:
+	  break;
+      case e_dv_sample_411:
+      case e_dv_sample_422:
+         /* libdv decodes NTSC DV (native 411) and by default also PAL
+          * DV (native 420) as packed YUV 422 (YUY2 or 4CC 0x32595559)
+          * where the U and V information is repeated.  This can be
+          * transformed to planar 420 (YV12 or 4CC 0x32315659).
+          * For NTSC DV this transformation is lossy.
+          */
+         pitches[0] = decoder->width * 2;
+         pitches[1] = 0;
+         pitches[2] = 0;
+
+#ifdef LIBDV_PRE_0_9_5
+	 dv_decode_full_frame(decoder, jpeg_buffer, e_dv_color_yuv,
+		(guchar **) dv_frame, pitches);
+#else
+	 dv_decode_full_frame(decoder, jpeg_buffer, e_dv_color_yuv,
+		dv_frame, pitches);
+#endif
+
+	 output[0] = settings->yuv_overlay->pixels[0];
+	 output[1] = settings->yuv_overlay->pixels[2];
+	 output[2] = settings->yuv_overlay->pixels[1];
+	 frame_YUV422_to_YUV420P(output, dv_frame[0],
+		decoder->width,	decoder->height);
+	 break;
+     }
+     break;
+	
 # endif
    default:
      return 0;
@@ -569,7 +722,6 @@ static int lavplay_SDL_update(lavplay_t *info, uint8_t *jpeg_buffer,
 
    if (!lavplay_SDL_unlock(info)) return 0;
    SDL_DisplayYUVOverlay(settings->yuv_overlay, &(settings->jpegdims));
-
    return 1;
 }
 #endif
@@ -1403,6 +1555,20 @@ static int lavplay_init(lavplay_t *info)
          return 0;
    }
 #endif
+
+#ifdef	SUPPORT_READ_DV2
+#ifdef LIBDV_PRE_0_9_5
+   decoder = dv_decoder_new();
+   dv_init();
+#else
+   decoder = dv_decoder_new(0,0,0);
+#endif
+   decoder->quality = DV_QUALITY_BEST;
+   dv_frame[0] = (uint8_t *)malloc(3 * /*param->output_width * param->output_height */ 720 * 480 * 4);
+   dv_frame[1] = NULL;
+   dv_frame[2] = NULL;
+#endif
+
 
    if (editlist->has_audio && info->audio)
    {
