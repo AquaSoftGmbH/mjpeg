4de848ff84bbd3fb9adc1e4aecd3d5d4bcfb1f1f
waldviertler <>
2003-01-26 08:35:22 +0000

    added some lines again

--- mjpeg_play/docs/install-info.sh
+++ mjpeg_play/docs/install-info.sh
@@ -7,11 +7,11 @@ if [ $? -eq 0 ]
 then
 	echo DEBIAN
   # Here we install the doku for debian style install-info
-  #install-info $1/mjpeg-howto.info --infodir==$1/dir --menuentry=\
-	#"* mjpeg-howto: (mjpeg-howto).        How to use the mjpeg tools"
+  install-info $1/mjpeg-howto.info --infodir==$1/dir --menuentry=\
+   "* mjpeg-howto: (mjpeg-howto).        How to use the mjpeg tools"
 else
   # Here we install the doku for gnu style install-info
   install-info $1/mjpeg-howto.info $1/dir --entry=\
-"* mjpeg-howto: (mjpeg-howto).        How to use the mjpeg tools"
+    "* mjpeg-howto: (mjpeg-howto).        How to use the mjpeg tools"
 fi
 
