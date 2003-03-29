65d56cfdc4eea900841274503a30e5bc5d1a25f4
wackston2 <>
2003-03-29 12:01:01 +0000

    At last... figured out how to get install-info working for Debian. Bad bad Debian using a non-standard install-info...

--- mjpeg_play/docs/install-info.sh
+++ mjpeg_play/docs/install-info.sh
@@ -8,9 +8,8 @@ install-info --help 2>&1 | grep menuentry >/dev/null
 
 if [ $? -eq 0 ]
 then
-	echo DEBIAN
   # Here we install the doku for debian style install-info
-  install-info $1/mjpeg-howto.info --infodir=$1/dir --menuentry="* mjpeg-howto: (mjpeg-howto).        How to use the mjpeg tools"
+  install-info  --infodir=$1 --menuentry="* mjpeg-howto: (mjpeg-howto).        How to use the mjpeg tools" --description="mjpeg tools documentation" $1/mjpeg-howto.info
 else
   # Here we install the doku for gnu style install-info
   install-info $1/mjpeg-howto.info $1/dir --entry="* mjpeg-howto: (mjpeg-howto).        How to use the mjpeg tools"
