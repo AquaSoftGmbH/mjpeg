3919bf1a49a19adab4f2583556218f3397ec627f
sms00 <>
2003-05-09 02:48:45 +0000

    Added the capability to place a black border with -b (same usage as yuvdenoise)

--- mjpeg_play/lavtools/y4mshift.c
+++ mjpeg_play/lavtools/y4mshift.c
@@ -3,10 +3,16 @@
  * $Id$
  *
  * written by Steven M. Schultz <sms@2BSD.COM>
- * Simple program to shift the data an even number of pixels.   The shift count
- * is positive for shifting to the right and negative for a left shift.
  *
- * Usage: y4mfill -n N
+ * 2003/5/8 - added the ability to place a black border around the frame.
+ *	The -b usage is the same as for yuvdenoise.   This is a useful 
+ *	feature when yuvdenoise or other programs are not being used but a
+ *	border is still desired.
+ *
+ * Simple program to shift the data an even number of pixels.   The shift count
+ * is positive for shifting to the right and negative for a left shift.   
+ *
+ * Usage: y4mfill -n N [ -b xoffset,yoffset,xsize,ysize ]
  *
  * No file arguments are needed since this is a filter only program.
 */
@@ -27,6 +33,7 @@ extern  char    *__progname;
 
 #define HALFSHIFT (shiftnum / 2)
 
+	void	black_border(u_char **, char *, int, int);
 static  void    usage();
 
 int main(int argc, char **argv)
@@ -35,16 +42,20 @@ int main(int argc, char **argv)
         int     shiftnum = 0, rightshift;
         int     verbose = 0, fdin;
         u_char  *yuv[3], *line;
+	char	*borderarg = NULL;
         y4m_stream_info_t istream, ostream;
         y4m_frame_info_t iframe;
 
         fdin = fileno(stdin);
 
         opterr = 0;
-        while   ((c = getopt(argc, argv, "hvn:")) != EOF)
+        while   ((c = getopt(argc, argv, "hvn:b:")) != EOF)
                 {
                 switch  (c)
                         {
+			case	'b':
+				borderarg = strdup(optarg);
+				break;
                         case    'n':
                                 shiftnum = atoi(optarg);
                                 break;
@@ -176,6 +187,8 @@ int main(int argc, char **argv)
                                 }
                         }
 outputframe:
+		if	(borderarg)
+			black_border(yuv, borderarg, width, height);
                 y4m_write_frame(fileno(stdout), &ostream, &iframe, yuv);
                 }
         y4m_fini_frame_info(&iframe);
@@ -185,18 +198,103 @@ outputframe:
         exit(0);
         }
 
+/*
+ * -b Xoff,Yoff,Xsize,YSize
+*/
+
+void black_border (u_char *yuv[], char *borderstring, int W, int H)
+	{
+	static	int parsed = -1;
+	static	int BX0, BX1;	/* Left, Right border columns */
+	static	int BY0, BY1;	/* Top, Bottom border rows */
+	int	i1, i2, i, dy, W2, H2;
+  
+	if	(parsed == -1)
+		{
+		parsed = 0;
+		i = sscanf(borderstring, "%d,%d,%d,%d", &BX0, &BY0, &i1, &i2);
+		if	(i != 4 || (BX0 % 2) || (BY0 % 4) || i1 < 0 || i2 < 0 ||
+			 (BX0 + i1 > W) || (BY0 + i2 > H))
+			{
+			mjpeg_log(LOG_WARN, " border args invalid - ignored");
+			return;
+			}
+		BX1 = BX0 + i1;
+		BY1 = BY0 + i2;
+		parsed = 1;
+		}
+/*
+ * If a borderstring was seen but declared invalid then it
+ * is being ignored so just return.
+*/
+	if	(parsed == 0)
+		return;
+
+	W2 = W / 2;
+	H2 = H / 2;
+
+/*
+ * Yoff Lines at the top.   If the vertical offset is 0 then no top border
+ * is being requested and there is nothing to do.
+*/
+	if	(BY0 != 0)
+		{
+		memset(yuv[0], 16,  W  * BY0);
+		memset(yuv[1], 128, W2 * BY0/2);
+		memset(yuv[2], 128, W2 * BY0/2);
+		}
+/*
+ * Height - (Ysize + Yoff) lines at bottom.   If the bottom coincides with
+ * the frame size then there is nothing to do.
+*/
+	if	(H != BY1)
+		{
+		memset(&yuv[0][BY1 * W], 16, W  * (H - BY1));
+		memset(&yuv[1][(BY1 / 2) * W2], 128, W2 * (H - BY1)/2);
+		memset(&yuv[2][(BY1 / 2) * W2], 128, W2 * (H - BY1)/2);
+		}
+/*
+ * Now the partial lines in the middle.   Go from rows BY0 thru BY1 because
+ * the whole rows (0 thru BY0) and (BY1 thru H) have been done above.
+*/
+	for	(dy = BY0; dy < BY1; dy++)
+		{
+/*
+ * First the columns on the left (x = 0 thru BX0).   If the X offset is 0
+ * then there is nothing to do.
+*/
+		if	(BX0 != 0)
+			{
+			memset(&yuv[0][dy * W], 16, BX0);
+			memset(&yuv[1][dy / 2 * W2], 128, BX0 / 2);
+			memset(&yuv[2][dy / 2 * W2], 128, BX0 / 2);
+			}
+/*
+ * Then the columns on the right (x = BX1 thru W).   If the right border
+ * start coincides with the frame size then there is nothing to do.
+*/
+		if	(W != BX1)
+			{
+			memset(&yuv[0][(dy * W) + BX1], 16, W - BX1);
+			memset(&yuv[1][(dy/2 * W2) + BX1/2], 128, (W - BX1)/2);
+			memset(&yuv[2][(dy/2 * W2) + BX1/2], 128, (W - BX1)/2);
+			}
+		}
+
+	}
 static void usage()
         {
 
-        fprintf(stderr, "%s: usage: [-v] [-h] -n N< in > out\n", __progname);
+        fprintf(stderr, "%s: usage: [-v] [-h] [-b xoff,yoff,xsize,ysize] -n N\n", __progname);
         fprintf(stderr, "%s:\tN = number of pixels to shift - must be even!!\n",
                 __progname);
         fprintf(stderr, "%s:\t\tpositive count means shift right\n",__progname);
         fprintf(stderr, "%s:\t\t0 passes the data thru unchanged\n",__progname);
         fprintf(stderr, "%s:\t\tnegative count means shift left\n", __progname);
+	fprintf(stderr, "%s:\t-b creates black border\n", __progname);
+	fprintf(stderr, "%s:\t\tusage is same as yuvdenoise.\n", __progname);
         fprintf(stderr, "%s:\t-v progress report every 100 frames\n", 
                 __progname);
         fprintf(stderr, "%s:\t-h print this usage summary\n", __progname);
         exit(1);
         }
-
