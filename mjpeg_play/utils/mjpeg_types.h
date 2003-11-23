4a9686a2ed02b3b2ff5fd7d08380e03b2ba65bb1
sms00 <>
2003-11-23 04:36:05 +0000

    clean up, remove unused cruft from mjpeg_types.h and win32defs.h, HAVE_STDBOOL_H goes away since none of the C code uses it (and C++ has it builtin).   mjpeg_types.h is a shadow of its former self ;)

--- mjpeg_play/utils/mjpeg_types.h
+++ mjpeg_play/utils/mjpeg_types.h
@@ -24,23 +24,15 @@
 
 #include <sys/types.h> /* FreeBSD, others - ssize_t */
 
+/*
+ * modern CYGWIN releases have stdint/inttypes so there's no need
+ * to check for it or provide typedefs/defines
+*/
+
 #if defined(HAVE_STDINT_H)
 # include <stdint.h>
 #elif defined(HAVE_INTTYPES_H)
 # include <inttypes.h>
-#elif defined(__CYGWIN__)
-typedef u_int8_t uint8_t;
-typedef u_int16_t uint16_t;
-typedef u_int32_t uint32_t;
-typedef u_int64_t uint64_t;
-# define INT8_C(c)     c
-# define INT16_C(c)    c
-# define INT32_C(c)    c
-# define INT64_C(c)    c ## LL
-# define UINT8_C(c)    c ## U
-# define UINT16_C(c)   c ## U
-# define UINT32_C(c)   c ## U
-# define UINT64_C(c)   c ## ULL
 #elif  defined(_WIN32)
 #include <win32defs.h>
 #else
@@ -59,25 +51,6 @@ script is a good example of how to do this. You need to check for \
 PRId64, stdbool.h, inttypes.h, stdint.h and sys/types.h
 #endif /* HAVE_STDINT_H */
 
-
-#if defined(HAVE_STDBOOL_H) && !defined(__cplusplus)
-#include <stdbool.h>
-#else
-/* ISO/IEC 9899:1999 <stdbool.h> missing -- enabling workaround */
-
-# ifndef __cplusplus
-typedef enum
-  {
-    false = 0,
-    true = 1
-  } locBool;
-
-#  define false   false
-#  define true    true
-#  define bool locBool
-# endif
-#endif
-
 #ifndef PRId64
 #define PRId64 PRID64_STRING_FORMAT
 #endif
@@ -85,28 +58,9 @@ typedef enum
 #if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ > 4)
 #define GNUC_PRINTF( format_idx, arg_idx )    \
   __attribute__((format (printf, format_idx, arg_idx)))
-#define GNUC_SCANF( format_idx, arg_idx )     \
-  __attribute__((format (scanf, format_idx, arg_idx)))
-#define GNUC_FORMAT( arg_idx )                \
-  __attribute__((format_arg (arg_idx)))
-#define GNUC_NORETURN                         \
-  __attribute__((noreturn))
-#define GNUC_CONST                            \
-  __attribute__((const))
-#define GNUC_UNUSED                           \
-  __attribute__((unused))
-#define GNUC_PACKED                           \
-  __attribute__((packed))
 #else   /* !__GNUC__ */
 #define GNUC_PRINTF( format_idx, arg_idx )
-#define GNUC_SCANF( format_idx, arg_idx )
-#define GNUC_FORMAT( arg_idx )
-#define GNUC_NORETURN
-#define GNUC_CONST
-#define GNUC_UNUSED
-#define GNUC_PACKED
 #endif  /* !__GNUC__ */
-
 
 #endif /* __MJPEG_TYPES_H__ */
 
