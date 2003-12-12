1238b72fd060a4401de1543ac950303cd5616225
sms00 <>
2003-12-12 06:43:53 +0000

    Ifdef _SC_NPROCESSORS_ONLN since neither OS/X nor BSD/OS have it (they use a sysctl based method).

--- mjpeg_play/mpeg2enc/mpeg2encoptions.cc
+++ mjpeg_play/mpeg2enc/mpeg2encoptions.cc
@@ -54,7 +54,11 @@ MPEG2EncOptions::MPEG2EncOptions()
     closed_GOPs = 0;
     preserve_B = 0;
     Bgrp_size = 3;
+#ifdef _SC_NPROCESSORS_ONLN
     num_cpus = sysconf (_SC_NPROCESSORS_ONLN);
+#else
+    num_cpus = 1;
+#endif
     if (num_cpus < 0)
       num_cpus = 1;
     if (num_cpus > 32)
