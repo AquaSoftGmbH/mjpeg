81e45f9487f26af9931484eba655a888eaccc345
ulatec <>
2004-04-27 20:45:56 +0000

    First GPL release of y4mdenoise.
    This still needs to be reviewed by an actual digital-video expert to
    make sure my assumptions are valid.

---
+++ mjpeg_play/y4mdenoise/main.c
@@ -0,0 +1,348 @@
+/***********************************************************
+ * A new denoiser for the mjpegtools project               *
+ * ------------------------------------------------------- *
+ * (C) 2004 Steven Boswell.                                *
+ * Based on yuvdenoise/main.c, (C) 2001,2002 Stefan Fendt  *
+ *                                                         *
+ * Licensed and protected by the GNU-General-Public-       *
+ * License version 2 or if you prefer any later version of *
+ * that license). See the file COPYING for detailed infor- *
+ * mation.                                                 *
+ ***********************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "mjpeg_types.h"
+#include "yuv4mpeg.h"
+#include "mjpeg_logging.h"
+#include "config.h"
+#include "newdenoise.hh"
+
+// HACK
+#include <sys/stat.h>
+#include <fcntl.h>
+
+void process_commandline(int argc, char *argv[]);
+void alloc_buffers (void);
+
+DNSR_GLOBAL denoiser;
+int frame = 0;
+char *g_pszInputFile = NULL;		// HACK
+
+/***********************************************************
+ *                                                         *
+ ***********************************************************/
+
+int main(int argc, char *argv[])
+{
+  int fd_in  = 0;
+  int fd_out = 1;
+  int errno  = 0;
+  
+  y4m_frame_info_t frameinfo;
+  y4m_stream_info_t streaminfo;
+  int chroma_mode;
+
+  frame = 0;
+
+  /* initialize stream-information */
+  y4m_accept_extensions (1);
+  y4m_init_stream_info (&streaminfo);
+  y4m_init_frame_info (&frameinfo);
+  
+  /* setup denoiser's global variables */
+  denoiser.skip               = 0;
+  denoiser.frames             = 10;
+  denoiser.interlaced         = -1;
+  denoiser.bwonly             = 0;
+  denoiser.radiusY            = 16;
+  denoiser.radiusCbCr         = -1;
+  denoiser.thresholdY         = 3; /* assume medium noise material */
+  denoiser.thresholdCbCr      = -1;
+  denoiser.matchCountThrottle = 5;
+  denoiser.matchSizeThrottle  = 5;
+  
+  /* process commandline */
+  process_commandline(argc, argv);
+  if (denoiser.radiusCbCr == -1)
+  	denoiser.radiusCbCr = denoiser.radiusY;
+  if (denoiser.thresholdCbCr == -1)
+  	denoiser.thresholdCbCr = denoiser.thresholdY;
+
+	/* HACK: open input file. */
+	if (g_pszInputFile != NULL)
+	{
+		fd_in = open (g_pszInputFile, O_RDONLY);
+		if (fd_in == -1)
+		{
+			mjpeg_log (LOG_ERROR, "Couldn't open input file %s: %s!",
+				g_pszInputFile, y4m_strerr (errno));
+			exit (1);
+		}
+	}
+
+	/* open input stream */
+	if ((errno = y4m_read_stream_header (fd_in, &streaminfo)) != Y4M_OK)
+		mjpeg_error_exit1 ("Couldn't read YUV4MPEG header: %s!",
+			y4m_strerr (errno));
+	if (y4m_si_get_plane_count (&streaminfo) != 3)
+		mjpeg_error_exit1 ("Only 3-plane formats supported.");
+	denoiser.frame.w = y4m_si_get_width (&streaminfo);
+	denoiser.frame.h = y4m_si_get_height (&streaminfo);
+	chroma_mode = y4m_si_get_chroma (&streaminfo);
+	denoiser.frame.ss_h = y4m_chroma_ss_x_ratio (chroma_mode).d;
+	denoiser.frame.ss_v = y4m_chroma_ss_y_ratio (chroma_mode).d;
+	denoiser.frame.Cw=denoiser.frame.w/denoiser.frame.ss_h;
+	denoiser.frame.Ch=denoiser.frame.h/denoiser.frame.ss_v;
+
+  /* write the outstream header */
+  y4m_write_stream_header (fd_out, &streaminfo); 
+
+  /* allocate memory for frames */
+  alloc_buffers();
+	
+  /* set interlacing, if it hasn't been yet. */
+  if (denoiser.interlaced == -1)
+  {
+    int n = y4m_si_get_interlace(&streaminfo);
+    switch (n)
+    {
+    case Y4M_ILACE_NONE:
+         denoiser.interlaced = 0;
+         break;
+    case Y4M_ILACE_TOP_FIRST:
+         denoiser.interlaced = 1;
+         break;
+    case Y4M_ILACE_BOTTOM_FIRST:
+         denoiser.interlaced = 2;
+         break;
+    default:
+         mjpeg_warn("Unknown interlacing '%d', assuming non-interlaced", n);
+         denoiser.interlaced = 0;
+         break;
+    }
+  }
+
+	/* initialize the denoiser */
+	errno = newdenoise_init (denoiser.frames, denoiser.frame.w,
+		denoiser.frame.h, (denoiser.bwonly) ? 0 : denoiser.frame.Cw,
+		(denoiser.bwonly) ? 0 : denoiser.frame.Ch);
+	if (errno == -1)
+		mjpeg_error_exit1( "Could not initialize denoiser");
+
+  /* read every single frame until the end of the input stream */
+  while 
+    (
+          Y4M_OK == ( errno = y4m_read_frame (fd_in, 
+                                              &streaminfo, 
+                                              &frameinfo, 
+                                              denoiser.frame.in) )
+    )
+    {
+	  frame++;
+	  //if (frame < 5395) continue;	// MAJOR HACK MAJOR HACK MAJOR HACK
+	  //if (frame == 5455) break;	// MAJOR HACK MAJOR HACK MAJOR HACK
+	  // fprintf (stderr, "Frame %d\r", frame);	// HACK
+	  // MAJOR HACK: cut out commercials
+	  //if ((frame < 54692)
+	  //|| (frame >= 20829 && frame <= 28231)
+	  //|| (frame >= 42737 && frame <= 45764)
+	  //|| (frame >= 68541 && frame <= 74895)
+	  //|| (frame >= 92217 && frame <= 95814))
+	  if (frame <= denoiser.skip)
+	  {
+         y4m_write_frame ( fd_out, 
+                        &streaminfo, 
+                        &frameinfo, 
+                        denoiser.frame.in);
+		 continue;
+	  }
+      
+		/* denoise the current frame */
+		errno = ((denoiser.interlaced == 0) ? newdenoise_frame
+			: newdenoise_interlaced_frame) (denoiser.frame.in[0],
+			denoiser.frame.in[1], denoiser.frame.in[2],
+			denoiser.frame.out[0], denoiser.frame.out[1],
+			denoiser.frame.out[2]);
+		if (errno == -1)
+			mjpeg_error_exit1( "Could not denoise frame %d", frame);
+	      
+		/* if b/w was selected, set the frame color to white */
+		if (denoiser.bwonly)
+		{
+			int i, iExtent = denoiser.frame.Cw * denoiser.frame.Ch;
+
+			for (i = 0; i < iExtent; ++i)
+				denoiser.frame.out[1][i] = 128;
+			for (i = 0; i < iExtent; ++i)
+				denoiser.frame.out[2][i] = 128;
+		}
+
+		/* if there was some output, write it. */
+		if (errno == 0)
+	    	y4m_write_frame ( fd_out, 
+	                        &streaminfo, 
+	                        &frameinfo, 
+	                        denoiser.frame.out);
+    }
+    
+  /* did stream end unexpectedly ? */
+  if(errno != Y4M_ERR_EOF && errno != Y4M_OK )
+          mjpeg_error_exit1( "%s", y4m_strerr( errno ) );
+  
+  /* write out any remaining frames */
+  for (;;)
+  {
+	frame++;
+	// fprintf (stderr, "Frame %d\r", frame);	// HACK
+	errno = ((denoiser.interlaced == 0) ? newdenoise_frame
+		: newdenoise_interlaced_frame) (NULL, NULL, NULL,
+		denoiser.frame.out[0], denoiser.frame.out[1],
+		denoiser.frame.out[2]);
+	if (errno == -1)
+		mjpeg_error_exit1( "Could not denoise frame %d", frame);
+    /* if there was no output, leave. */
+	if (errno == 1)
+		break;
+
+	/* if b/w was selected, set the frame color to white */
+	if (denoiser.bwonly)
+	{
+		int i, iExtent = denoiser.frame.Cw * denoiser.frame.Ch;
+
+		for (i = 0; i < iExtent; ++i)
+			denoiser.frame.out[1][i] = 128;
+		for (i = 0; i < iExtent; ++i)
+			denoiser.frame.out[2][i] = 128;
+	}
+
+	/* if there was some output, write it. */
+	if (errno == 0)
+    	y4m_write_frame ( fd_out, 
+                        &streaminfo, 
+                        &frameinfo, 
+                        denoiser.frame.out);
+  }
+
+  /* Exit gently */
+  return(0);
+}
+
+
+
+void
+process_commandline(int argc, char *argv[])
+{
+  char c;
+
+  while ((c = getopt (argc, argv, "t:T:r:R:m:M:s:f:BI:i:")) != -1)	// HACK
+  {
+    switch (c)
+    {
+	  case 'i':	// HACK
+	  {
+	  	g_pszInputFile = optarg;
+		break;
+	  }
+      case 'r':
+      {
+        denoiser.radiusY = atoi(optarg);
+        if(denoiser.radiusY<4)
+        {
+          denoiser.radiusY=4;
+  	      mjpeg_log (LOG_WARN, "Minimum allowed search radius is 4 pixels.");
+        }
+        break;
+	  }
+      case 'R':
+      {
+        denoiser.radiusCbCr = atoi(optarg);
+        if(denoiser.radiusCbCr<4)
+        {
+          denoiser.radiusCbCr=4;
+  	      mjpeg_log (LOG_WARN, "Minimum allowed color search radius is 4 pixel.");
+        }
+        break;
+      }
+      case 't':
+      {
+        denoiser.thresholdY = atoi(optarg);
+        break;
+      }
+      case 'T':
+      {
+        denoiser.thresholdCbCr = atoi(optarg);
+        break;
+      }
+      case 'm':
+      {
+        denoiser.matchCountThrottle = atoi(optarg);
+        break;
+      }
+      case 'M':
+      {
+        denoiser.matchSizeThrottle = atoi(optarg);
+        break;
+      }
+      case 's':
+      {
+        denoiser.skip = atoi(optarg);
+        break;
+      }
+      case 'f':
+      {
+        denoiser.frames = atoi(optarg);
+        break;
+      }
+      case 'B':
+      {
+        denoiser.bwonly = 1;
+        break;
+      }
+      case 'I':
+      {
+	 	int interlaced = atoi (optarg);
+		if (interlaced != 0 && interlaced != 1 && interlaced != 2)
+		{
+      		mjpeg_log (LOG_ERROR, "-I must be either 0, 1, or 2");
+			exit (1);
+		}
+        denoiser.interlaced = interlaced;
+        break;
+      }
+    }
+  }
+}
+
+
+void alloc_buffers(void)
+{
+  int luma_buffsize; 
+  int chroma_buffsize;
+
+  luma_buffsize = denoiser.frame.w * denoiser.frame.h;
+  chroma_buffsize = denoiser.frame.Cw * denoiser.frame.Ch;
+  
+  denoiser.frame.in[0] = (uint8_t *) malloc (luma_buffsize);
+  denoiser.frame.in[1] = (uint8_t *) malloc (chroma_buffsize);
+  denoiser.frame.in[2] = (uint8_t *) malloc (chroma_buffsize);
+  
+  denoiser.frame.out[0] = (uint8_t *) malloc (luma_buffsize);
+  denoiser.frame.out[1] = (uint8_t *) malloc (chroma_buffsize);
+  denoiser.frame.out[2] = (uint8_t *) malloc (chroma_buffsize);
+
+	if ( denoiser.frame.in[0] == NULL
+	|| denoiser.frame.in[1] == NULL
+	|| denoiser.frame.in[2] == NULL
+	|| denoiser.frame.out[0] == NULL
+	|| denoiser.frame.out[1] == NULL
+	|| denoiser.frame.out[2] == NULL)
+		mjpeg_error_exit1( "Out of memory "
+			"when allocating frame buffers");
+}
---
+++ mjpeg_play/y4mdenoise/newdenoise.cc
@@ -0,0 +1,509 @@
+// This file (C) 2004 Steven Boswell.  All rights reserved.
+// Released to the public under the GNU General Public License.
+// See the file COPYING for more information.
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include "mjpeg_types.h"
+#include <stdio.h>
+#include "newdenoise.hh"
+#include "MotionSearcher.hh"
+
+// The denoisers.  (We have to make these classes, in order to keep gdb
+// from crashing.  I didn't even know one could crash gdb. ;-)
+#if 0
+typedef MotionSearcher<uint8_t, 1, int32_t, int16_t, int32_t, 4, 2,
+		uint16_t, PixelY, ReferencePixelY, ReferenceFrameY>
+	MotionSearcherY;
+typedef MotionSearcher<uint8_t, 2, int32_t, int16_t, int32_t, 2, 2,
+		uint16_t, PixelCbCr, ReferencePixelCbCr, ReferenceFrameCbCr>
+	MotionSearcherCbCr;
+#else
+class MotionSearcherY
+	: public MotionSearcher<uint8_t, 1, int32_t, int16_t, int32_t, 4, 2,
+		uint16_t, PixelY, ReferencePixelY, ReferenceFrameY> {};
+class MotionSearcherCbCr
+	: public MotionSearcher<uint8_t, 2, int32_t, int16_t, int32_t, 2, 2,
+		uint16_t, PixelCbCr, ReferencePixelCbCr, ReferenceFrameCbCr> {};
+#endif
+MotionSearcherY g_oMotionSearcherY;
+MotionSearcherCbCr g_oMotionSearcherCbCr;
+
+// Whether the denoisers should be used.
+bool g_bMotionSearcherY;
+bool g_bMotionSearcherCbCr;
+
+// Pixel buffers, used to translate provided input into the form the
+// denoiser needs.
+int g_nPixelsY, g_nWidthY, g_nHeightY;
+MotionSearcherY::Pixel_t *g_pPixelsY;
+int g_nPixelsCbCr, g_nWidthCbCr, g_nHeightCbCr;
+MotionSearcherCbCr::Pixel_t *g_pPixelsCbCr;
+
+// An internal method to output a frame.
+static void output_frame
+	(const MotionSearcherY::ReferenceFrame_t *a_pFrameY,
+	const MotionSearcherCbCr::ReferenceFrame_t *a_pFrameCbCr,
+	uint8_t *a_pOutputY, uint8_t *a_pOutputCb, uint8_t *a_pOutputCr);
+static void output_field
+	(int a_nMask, const MotionSearcherY::ReferenceFrame_t *a_pFrameY,
+	const MotionSearcherCbCr::ReferenceFrame_t *a_pFrameCbCr,
+	uint8_t *a_pOutputY, uint8_t *a_pOutputCb, uint8_t *a_pOutputCr);
+
+int newdenoise_init (int a_nFrames, int a_nWidthY, int a_nHeightY,
+	int a_nWidthCbCr, int a_nHeightCbCr)
+{
+	Status_t eStatus;
+		// An error that may occur.
+	int nInterlace;
+		// A factor to apply to frames/frame-height because of
+		// interlacing.
+	
+	// No errors yet.
+	eStatus = g_kNoError;
+
+	// Save the width and height.
+	g_nWidthY = a_nWidthY;
+	g_nHeightY = a_nHeightY;
+	g_nWidthCbCr = a_nWidthCbCr;
+	g_nHeightCbCr = a_nHeightCbCr;
+
+	// If the video is interlaced, that means the denoiser will see
+	// twice as many frames, half their original height.
+	nInterlace = (denoiser.interlaced) ? 2 : 1;
+
+	// If intensity should be denoised, set it up.
+	if (a_nWidthY != 0 && a_nHeightY != 0)
+	{
+		g_bMotionSearcherY = true;
+		g_nPixelsY = a_nWidthY * a_nHeightY / nInterlace;
+		g_pPixelsY = new MotionSearcherY::Pixel_t [g_nPixelsY];
+		if (g_pPixelsY == NULL)
+			return -1;
+		g_oMotionSearcherY.Init (eStatus, nInterlace * a_nFrames,
+			a_nWidthY, a_nHeightY / nInterlace,
+			denoiser.radiusY, denoiser.radiusY,
+			denoiser.thresholdY, denoiser.matchCountThrottle,
+			denoiser.matchSizeThrottle);
+		if (eStatus != g_kNoError)
+		{
+			delete[] g_pPixelsY;
+			return -1;
+		}
+	}
+	else
+		g_bMotionSearcherY = false;
+
+	// If color should be denoised, set it up.
+	if (a_nWidthCbCr != 0 && a_nHeightCbCr != 0)
+	{
+		g_bMotionSearcherCbCr = true;
+		g_nPixelsCbCr = a_nWidthCbCr * a_nHeightCbCr / nInterlace;
+		g_pPixelsCbCr = new MotionSearcherCbCr::Pixel_t [g_nPixelsCbCr];
+		if (g_pPixelsCbCr == NULL)
+			return -1;
+		g_oMotionSearcherCbCr.Init (eStatus, nInterlace * a_nFrames,
+			a_nWidthCbCr, a_nHeightCbCr / nInterlace,
+			denoiser.radiusCbCr / denoiser.frame.ss_h,
+			denoiser.radiusCbCr / denoiser.frame.ss_v,
+			denoiser.thresholdCbCr,
+			denoiser.matchCountThrottle, denoiser.matchSizeThrottle);
+		if (eStatus != g_kNoError)
+			return -1;
+	}
+	else
+		g_bMotionSearcherCbCr = false;
+
+	// Initialization was successful.
+	return 0;
+}
+
+int
+newdenoise_frame (const uint8_t *a_pInputY, const uint8_t *a_pInputCb,
+	const uint8_t *a_pInputCr, uint8_t *a_pOutputY,
+	uint8_t *a_pOutputCb, uint8_t *a_pOutputCr)
+{
+	Status_t eStatus;
+		// An error that may occur.
+	const MotionSearcherY::ReferenceFrame_t *pFrameY;
+	const MotionSearcherCbCr::ReferenceFrame_t *pFrameCbCr;
+		// Denoised frame data, ready for output.
+	int i;
+		// Used to loop through pixels.
+
+	// No errors yet.
+	eStatus = g_kNoError;
+
+	// No output frames have been received yet.
+	pFrameY = NULL;
+	pFrameCbCr = NULL;
+
+	// If it's time to purge, do so.
+	{
+		extern int frame;
+		if (frame % 100 == 0)
+		{
+			g_oMotionSearcherY.Purge();
+			g_oMotionSearcherCbCr.Purge();
+		}
+	}
+
+	// If the end of input has been reached, then return the next
+	// remaining denoised frame, if available.
+	if ((g_bMotionSearcherY && a_pInputY == NULL)
+		|| (g_bMotionSearcherCbCr && a_pInputCr == NULL))
+	{
+		// Get any remaining frame.
+		if (g_bMotionSearcherY)
+			pFrameY = g_oMotionSearcherY.GetRemainingFrames();
+		if (g_bMotionSearcherCbCr)
+			pFrameCbCr = g_oMotionSearcherCbCr.GetRemainingFrames();
+	
+		// Output it.
+		output_frame (pFrameY, pFrameCbCr, a_pOutputY, a_pOutputCb,
+			a_pOutputCr);
+	}
+
+	// Otherwise, if there is more input, feed the frame into the
+	// denoiser & possibly get a frame back.
+	else
+	{
+		// Get any frame that's ready for output.
+		if (g_bMotionSearcherY)
+			pFrameY = g_oMotionSearcherY.GetFrameReadyForOutput();
+		if (g_bMotionSearcherCbCr)
+			pFrameCbCr = g_oMotionSearcherCbCr.GetFrameReadyForOutput();
+	
+		// Output it.
+		output_frame (pFrameY, pFrameCbCr, a_pOutputY, a_pOutputCb,
+			a_pOutputCr);
+	
+		// Pass the input frame to the denoiser.
+		if (g_bMotionSearcherY)
+		{
+			// Convert the input frame into the format needed by the
+			// denoiser.  (This step is a big waste of time & cache.
+			// I wish there was another way.)
+			for (i = 0; i < g_nPixelsY; ++i)
+				g_pPixelsY[i] = PixelY (a_pInputY + i);
+
+			// Pass the frame to the denoiser.
+			g_oMotionSearcherY.AddFrame (eStatus, g_pPixelsY);
+			if (eStatus != g_kNoError)
+				return -1;
+		}
+		if (g_bMotionSearcherCbCr)
+		{
+			PixelCbCr::Num_t aCbCr[2];
+
+			// Convert the input frame into the format needed by the
+			// denoiser.  (This step is a big waste of time & cache.
+			// I wish there was another way.)
+			for (i = 0; i < g_nPixelsCbCr; ++i)
+			{
+				aCbCr[0] = a_pInputCb[i];
+				aCbCr[1] = a_pInputCr[i];
+				g_pPixelsCbCr[i] = PixelCbCr (aCbCr);
+			}
+
+			// Pass the frame to the denoiser.
+			g_oMotionSearcherCbCr.AddFrame (eStatus, g_pPixelsCbCr);
+			if (eStatus != g_kNoError)
+				return -1;
+		}
+	}
+
+	// If we're denoising both color & intensity, make sure we
+	// either got two reference frames or none at all.
+	assert (!g_bMotionSearcherY || !g_bMotionSearcherCbCr
+		|| (pFrameY == NULL && pFrameCbCr == NULL)
+		|| (pFrameY != NULL && pFrameCbCr != NULL));
+
+	// Return whether there was an output frame this time.
+	return (g_bMotionSearcherY && pFrameY != NULL
+		|| g_bMotionSearcherCbCr && pFrameCbCr != NULL) ? 0 : 1;
+}
+
+static void output_frame
+	(const MotionSearcherY::ReferenceFrame_t *a_pFrameY,
+	const MotionSearcherCbCr::ReferenceFrame_t *a_pFrameCbCr,
+	uint8_t *a_pOutputY, uint8_t *a_pOutputCb, uint8_t *a_pOutputCr)
+{
+	int i;
+		// Used to loop through pixels.
+
+	// Convert any denoised intensity frame into the format expected
+	// by our caller.
+	if (a_pFrameY != NULL)
+	{
+		ReferencePixelY *pY;
+			// The pixel, as it's being converted to the output format.
+
+		// Make sure our caller gave us somewhere to write output.
+		assert (a_pOutputY != NULL);
+
+		// Loop through all the pixels, convert them to the output
+		// format.
+		for (i = 0; i < g_nPixelsY; ++i)
+		{
+			pY = a_pFrameY->GetPixel (i);
+			assert (pY != NULL);
+			a_pOutputY[i] = pY->GetValue()[0];
+		}
+	}
+	if (a_pFrameCbCr != NULL)
+	{
+		ReferencePixelCbCr *pCbCr;
+			// The pixel, as it's being converted to the output format.
+
+		// Make sure our caller gave us somewhere to write output.
+		assert (a_pOutputCb != NULL && a_pOutputCr != NULL);
+
+		// Loop through all the pixels, convert them to the output
+		// format.
+		for (i = 0; i < g_nPixelsCbCr; ++i)
+		{
+			pCbCr = a_pFrameCbCr->GetPixel (i);
+			assert (pCbCr != NULL);
+			const PixelCbCr &rCbCr = pCbCr->GetValue();
+			a_pOutputCb[i] = rCbCr[0];
+			a_pOutputCr[i] = rCbCr[1];
+		}
+	}
+}
+
+int
+newdenoise_interlaced_frame (const uint8_t *a_pInputY,
+	const uint8_t *a_pInputCb, const uint8_t *a_pInputCr,
+	uint8_t *a_pOutputY, uint8_t *a_pOutputCb, uint8_t *a_pOutputCr)
+{
+	Status_t eStatus;
+		// An error that may occur.
+	const MotionSearcherY::ReferenceFrame_t *pFrameY;
+	const MotionSearcherCbCr::ReferenceFrame_t *pFrameCbCr;
+		// Denoised frame data, ready for output.
+	int i, x, y;
+		// Used to loop through pixels.
+	int nMask;
+		// Used to switch between top-field interlacing and bottom-field
+		// interlacing.
+
+	// No errors yet.
+	eStatus = g_kNoError;
+
+	// No output frames have been received yet.
+	pFrameY = NULL;
+	pFrameCbCr = NULL;
+
+	// If it's time to purge, do so.
+	{
+		extern int frame;
+		if (frame % 100 == 0)
+		{
+			g_oMotionSearcherY.Purge();
+			g_oMotionSearcherCbCr.Purge();
+		}
+	}
+
+	// Set up for the type of interlacing.
+	nMask = (denoiser.interlaced == 2) ? 1 : 0;
+
+	// If the end of input has been reached, then return the next
+	// remaining denoised frame, if available.
+	if ((g_bMotionSearcherY && a_pInputY == NULL)
+		|| (g_bMotionSearcherCbCr && a_pInputCr == NULL))
+	{
+		// Get 1/2 any remaining frame.
+		if (g_bMotionSearcherY)
+			pFrameY = g_oMotionSearcherY.GetRemainingFrames();
+		if (g_bMotionSearcherCbCr)
+			pFrameCbCr = g_oMotionSearcherCbCr.GetRemainingFrames();
+	
+		// Output it.
+		output_field (nMask ^ 0, pFrameY, pFrameCbCr, a_pOutputY,
+			a_pOutputCb, a_pOutputCr);
+
+		// Get 1/2 any remaining frame.
+		if (g_bMotionSearcherY)
+			pFrameY = g_oMotionSearcherY.GetRemainingFrames();
+		if (g_bMotionSearcherCbCr)
+			pFrameCbCr = g_oMotionSearcherCbCr.GetRemainingFrames();
+	
+		// Output it.
+		output_field (nMask ^ 1, pFrameY, pFrameCbCr, a_pOutputY,
+			a_pOutputCb, a_pOutputCr);
+	}
+
+	// Otherwise, if there is more input, feed the frame into the
+	// denoiser & possibly get a frame back.
+	else
+	{
+		// Get 1/2 any frame that's ready for output.
+		if (g_bMotionSearcherY)
+			pFrameY = g_oMotionSearcherY.GetFrameReadyForOutput();
+		if (g_bMotionSearcherCbCr)
+			pFrameCbCr = g_oMotionSearcherCbCr.GetFrameReadyForOutput();
+	
+		// Output it.
+		output_field (nMask ^ 0, pFrameY, pFrameCbCr, a_pOutputY,
+			a_pOutputCb, a_pOutputCr);
+	
+		// Pass the input frame to the denoiser.
+		if (g_bMotionSearcherY)
+		{
+			// Convert the input frame into the format needed by the
+			// denoiser.
+			for (i = 0, y = (nMask ^ 0); y < g_nHeightY; y += 2)
+				for (x = 0; x < g_nWidthY; ++x, ++i)
+					g_pPixelsY[i]
+						= PixelY (a_pInputY + (y * g_nWidthY + x));
+			assert (i == g_nPixelsY);
+
+			// Pass the frame to the denoiser.
+			g_oMotionSearcherY.AddFrame (eStatus, g_pPixelsY);
+			if (eStatus != g_kNoError)
+				return -1;
+		}
+		if (g_bMotionSearcherCbCr)
+		{
+			PixelCbCr::Num_t aCbCr[2];
+
+			// Convert the input frame into the format needed by the
+			// denoiser.
+			for (i = 0, y = (nMask ^ 0); y < g_nHeightCbCr; y += 2)
+			{
+				for (x = 0; x < g_nWidthCbCr; ++x, ++i)
+				{
+					aCbCr[0] = a_pInputCb[y * g_nWidthCbCr + x];
+					aCbCr[1] = a_pInputCr[y * g_nWidthCbCr + x];
+					g_pPixelsCbCr[i] = PixelCbCr (aCbCr);
+				}
+			}
+			assert (i == g_nPixelsCbCr);
+
+			// Pass the frame to the denoiser.
+			g_oMotionSearcherCbCr.AddFrame (eStatus, g_pPixelsCbCr);
+			if (eStatus != g_kNoError)
+				return -1;
+		}
+	
+		// Get 1/2 any frame that's ready for output.
+		if (g_bMotionSearcherY)
+			pFrameY = g_oMotionSearcherY.GetFrameReadyForOutput();
+		if (g_bMotionSearcherCbCr)
+			pFrameCbCr = g_oMotionSearcherCbCr.GetFrameReadyForOutput();
+	
+		// Output it.
+		output_field (nMask ^ 1, pFrameY, pFrameCbCr, a_pOutputY,
+			a_pOutputCb, a_pOutputCr);
+	
+		// Pass the input frame to the denoiser.
+		if (g_bMotionSearcherY)
+		{
+			// Convert the input frame into the format needed by the
+			// denoiser.
+			for (i = 0, y = (nMask ^ 1); y < g_nHeightY; y += 2)
+				for (x = 0; x < g_nWidthY; ++x, ++i)
+					g_pPixelsY[i]
+						= PixelY (a_pInputY + (y * g_nWidthY + x));
+			assert (i == g_nPixelsY);
+
+			// Pass the frame to the denoiser.
+			g_oMotionSearcherY.AddFrame (eStatus, g_pPixelsY);
+			if (eStatus != g_kNoError)
+				return -1;
+		}
+		if (g_bMotionSearcherCbCr)
+		{
+			PixelCbCr::Num_t aCbCr[2];
+
+			// Convert the input frame into the format needed by the
+			// denoiser.
+			for (i = 0, y = (nMask ^ 1); y < g_nHeightCbCr; y += 2)
+			{
+				for (x = 0; x < g_nWidthCbCr; ++x, ++i)
+				{
+					aCbCr[0] = a_pInputCb[y * g_nWidthCbCr + x];
+					aCbCr[1] = a_pInputCr[y * g_nWidthCbCr + x];
+					g_pPixelsCbCr[i] = PixelCbCr (aCbCr);
+				}
+			}
+			assert (i == g_nPixelsCbCr);
+
+			// Pass the frame to the denoiser.
+			g_oMotionSearcherCbCr.AddFrame (eStatus, g_pPixelsCbCr);
+			if (eStatus != g_kNoError)
+				return -1;
+		}
+	}
+
+	// If we're denoising both color & intensity, make sure we
+	// either got two reference frames or none at all.
+	assert (!g_bMotionSearcherY || !g_bMotionSearcherCbCr
+		|| (pFrameY == NULL && pFrameCbCr == NULL)
+		|| (pFrameY != NULL && pFrameCbCr != NULL));
+
+	// Return whether there was an output frame this time.
+	return (g_bMotionSearcherY && pFrameY != NULL
+		|| g_bMotionSearcherCbCr && pFrameCbCr != NULL) ? 0 : 1;
+}
+
+static void output_field
+	(int a_nMask, const MotionSearcherY::ReferenceFrame_t *a_pFrameY,
+	const MotionSearcherCbCr::ReferenceFrame_t *a_pFrameCbCr,
+	uint8_t *a_pOutputY, uint8_t *a_pOutputCb, uint8_t *a_pOutputCr)
+{
+	int i, x, y;
+		// Used to loop through pixels.
+
+	// Convert any denoised intensity frame into the format expected
+	// by our caller.
+	if (a_pFrameY != NULL)
+	{
+		ReferencePixelY *pY;
+			// The pixel, as it's being converted to the output format.
+
+		// Make sure our caller gave us somewhere to write output.
+		assert (a_pOutputY != NULL);
+
+		// Loop through all the pixels, convert them to the output
+		// format.
+		for (i = 0, y = a_nMask; y < g_nHeightY; y += 2)
+		{
+			for (x = 0; x < g_nWidthY; ++x, ++i)
+			{
+				pY = a_pFrameY->GetPixel (i);
+				assert (pY != NULL);
+				a_pOutputY[y * g_nWidthY + x] = pY->GetValue()[0];
+			}
+		}
+	}
+	if (a_pFrameCbCr != NULL)
+	{
+		ReferencePixelCbCr *pCbCr;
+			// The pixel, as it's being converted to the output format.
+
+		// Make sure our caller gave us somewhere to write output.
+		assert (a_pOutputCb != NULL && a_pOutputCr != NULL);
+
+		// Loop through all the pixels, convert them to the output
+		// format.
+		for (i = 0, y = a_nMask; y < g_nHeightCbCr; y += 2)
+		{
+			for (x = 0; x < g_nWidthCbCr; ++x, ++i)
+			{
+				pCbCr = a_pFrameCbCr->GetPixel (i);
+				assert (pCbCr != NULL);
+				const PixelCbCr &rCbCr = pCbCr->GetValue();
+				a_pOutputCb[y * g_nWidthCbCr + x] = rCbCr[0];
+				a_pOutputCr[y * g_nWidthCbCr + x] = rCbCr[1];
+			}
+		}
+	}
+}
