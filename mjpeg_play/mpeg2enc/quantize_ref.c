1a9e47e959a06763756d09ee0bced5bfda65bf5d
trckjunky <>
2004-12-22 06:21:07 +0000

    get rid of SSE quant_non_intra (which was broken, and abusing program-wide SSE rounding flags which affected the ability to write other SSE routines).  Totally rewrite MMX quant_non_intra so that it is 1) correct, and 2) faster than the SSE code was!

--- mjpeg_play/mpeg2enc/quantize_ref.c
+++ mjpeg_play/mpeg2enc/quantize_ref.c
@@ -68,7 +68,8 @@ void enable_altivec_quantization(struct QuantizerCalls *calls, int opt_mpeg1);
 #endif
 #if defined(HAVE_ASM_MMX) && defined(HAVE_ASM_NASM)
 void init_x86_quantization( struct QuantizerCalls *calls,
-                                                        int mpeg1 );
+                            struct QuantizerWorkSpace *wsp,
+                            int mpeg1 );
 #endif
 
 #define fabsshift ((8*sizeof(unsigned int))-1)
@@ -504,6 +505,13 @@ void init_quantizer( struct QuantizerCalls *calls,
         {
             wsp->intra_q_tbl[q][i] = intra_q[i] * q;
             wsp->inter_q_tbl[q][i] = inter_q[i] * q;
+
+            if( wsp->intra_q_tbl[q][i] < 3 )
+                mjpeg_warn("Intra table falls below 3: q=%d, i=%d, v=%d",q,i,wsp->intra_q_tbl[q][i]);
+
+            if( wsp->inter_q_tbl[q][i] < 3 )
+                mjpeg_warn("Inter table falls below 3: q=%d, i=%d, v=%d",q,i,wsp->inter_q_tbl[q][i]);
+
             wsp->intra_q_tblf[q][i] = (float)wsp->intra_q_tbl[q][i];
             wsp->inter_q_tblf[q][i] = (float)wsp->inter_q_tbl[q][i];
             wsp->i_intra_q_tblf[q][i] = (float)(1.0 / (wsp->intra_q_tblf[q][i]));
@@ -532,7 +540,7 @@ void init_quantizer( struct QuantizerCalls *calls,
 #if defined(HAVE_ASM_MMX) && defined(HAVE_ASM_NASM)
     if( cpu_accel() )
     {
-        init_x86_quantization( calls, mpeg1 );
+        init_x86_quantization( calls, wsp, mpeg1 );
     }
 #endif
 #ifdef HAVE_ALTIVEC
--- mjpeg_play/mpeg2enc/quantize_x86.c
+++ mjpeg_play/mpeg2enc/quantize_x86.c
@@ -27,6 +27,7 @@
 #include <config.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 #ifdef HAVE_FENV_H
 #include <fenv.h>
 #endif
@@ -41,13 +42,6 @@
 #include "quantize_ref.h"
 
 /* Implemented in pure (NASM) assembler routines 	*/
-
-int quantize_non_intra_mb_mmx( int16_t *dst, int16_t *src, 
-							   uint16_t *quant_mat, 
-							   uint16_t *i_quant_mat, 
-							   int imquant, int mquant, 
-							   int sat_limit) 
-	__asm__ ("quantize_non_intra_mb_mmx");
 					
 int quant_weight_coeff_sum_mmx (int16_t *blk, uint16_t *i_quant_mat )
 	__asm__ ("quant_weight_coeff_sum_mmx");
@@ -224,243 +218,144 @@ restart:
 	return nzflag;
 }
 
-/*
- * SSE version: simply truncates to zero, however, the tables have a 2% bias
- * upwards which partly compensates.
- */
-static int trunc_mxcsr = 0x7f80;
- 
-static int quant_non_intra_sse( struct QuantizerWorkSpace *wsp,
-								int16_t *src, int16_t *dst,
-								int q_scale_type,
-								int satlim,
-								int *nonsat_mquant)
-{
-	int saturated;
-	float *i_quant_matf; 
-	int mquant = *nonsat_mquant;
-	int   coeff_count = 64*BLOCK_COUNT;
-	uint32_t nzflag, flags;
-	int16_t *psrc, *pdst;
-	float *piqf;
-	int i;
-	uint32_t tmp;
+/* MMX version, that is 100% accurate
+   It works by multiplying by the inverse of the quant factor.  However
+   in certain circumstances this results in a value too low.  To compensate,
+   it then multiplies by the quant factor, and compares this to the original
+   value.  If this new value is less than or equal to the original value
+   minus the quant factor, then the intermediate result is incremented.
+   This adjust-by-1 is all that is needed to correct the result.
 
-	/* Initialise zero block flags */
-	/* Load 1 into mm6 */
-	__asm__ ( "movl %0, %%eax\n" 
-			  "movd %%eax, %%mm6\n"
-			  : :"g" (1) : "eax" );
-	/* Set up SSE rounding mode */
-	__asm__ ( "ldmxcsr %0\n" : : "X" (trunc_mxcsr) );
-
-	/* Load satlim into mm1 */
-	movd_m2r( satlim, mm1 );
-	punpcklwd_r2r( mm1, mm1 );
-	punpckldq_r2r( mm1, mm1 );
-restart:
-	i_quant_matf = wsp->i_inter_q_tblf[mquant];
-	flags = 0;
-	piqf = i_quant_matf;
-	saturated = 0;
-	nzflag = 0;
-	psrc = src;
-	pdst = dst;
-	for (i=0; i < coeff_count ; i+=4)
-	{
-
-		/* Load 4 words, unpack into mm2 and mm3 (with sign extension!)
-		 */
-
-		movq_m2r( *(mmx_t *)&psrc[0], mm2 );
-		movq_r2r( mm2, mm7 );
-		psraw_i2r( 16, mm7 );	/* Replicate sign bits mm2 in mm7 */
-		movq_r2r( mm2, mm3 );
-		punpcklwd_r2r( mm7, mm2 ); /* Unpack with sign extensions */
-		punpckhwd_r2r( mm7, mm3);
-
-		/* Multiply by sixteen... */
-		pslld_i2r( 4, mm2 );
-		pslld_i2r( 4, mm3 );
-		
-		/*
-		  Convert mm2 and mm3 to float's  in xmm2 and xmm3
-		 */
-		cvtpi2ps_r2r( mm2, xmm2 );
-		cvtpi2ps_r2r( mm3, xmm3 );
-		shufps_r2ri(  xmm3, xmm2, 0*1 + 1*4 + 0 * 16 + 1 * 64 );
-
-		/* "Divide" by multiplying by inverse quantisation
-		 and convert back to integers*/
-		mulps_m2r( *(mmx_t*)&piqf[0], xmm2 );
-		cvtps2pi_r2r( xmm2, mm2 );
-		shufps_r2ri( xmm2, xmm2, 2*1 + 3*4 + 0 * 16 + 1 * 64 );
-		cvtps2pi_r2r( xmm2, mm3 );
-
-		/* Convert the two pairs of double words into four words */
-		packssdw_r2r(  mm3, mm2);
-
-
-		/* Accumulate saturation... */
-		movq_r2r( mm2, mm4 );
-
-		pxor_r2r( mm5, mm5 );	// mm5 = -mm2
-		pcmpgtw_r2r( mm1, mm4 ); // mm4 = (mm2 > satlim) 
-		psubw_r2r( mm2, mm5 );
-		pcmpgtw_r2r( mm1, mm5 ); // mm5 = -mm2 > satlim
-		por_r2r( mm5, mm4 );  // mm4 = abs(mm2) > satlim
-		movq_r2r( mm4, mm5 );
-		psrlq_i2r( 32, mm5);
-		por_r2r( mm5, mm4 );
-
-		movd_m2r( saturated, mm5 ); // saturated |= mm4
-		por_r2r( mm4, mm5 );
-		movd_r2m( mm5, saturated );
-
-		/* Store and accumulate zero-ness */
-		movq_r2r( mm2, mm3 );
-		movq_r2m( mm2, *(mmx_t*)pdst );
-		psrlq_i2r( 32, mm3 );
-		por_r2r( mm3, mm2 );
-		movd_r2m( mm2, tmp );
-		flags |= tmp;
-				
-		piqf += 4;
-		pdst += 4;
-		psrc += 4;
-
-		if( (i & 63) == (63/4)*4 )
-		{
-
-			if( saturated )
-			{
-				int new_mquant = next_larger_quant( q_scale_type, mquant );
-				if( new_mquant != mquant )
-				{
-					mquant = new_mquant;
-					goto restart;
-				}
-				else
-				{
-					return quant_non_intra(wsp, src, dst, 
-										   q_scale_type,
-										   satlim,
-										   nonsat_mquant);
-				}
-			}
-
-			nzflag = (nzflag<<1) | !!flags;
-			flags = 0;
-			piqf = i_quant_matf;
-		}
-			
-	}
-	emms();
-
-	//nzflag = (nzflag<<1) | (!!flags);
-	return nzflag;
-}
-
-/*
- * The ordinary MMX version.  Due to the limited dynamic range
- * afforded by working with 16-bit int's it (a) has to jump through
- * some gory fudge-factor hoops (b) give up in tough cases and fall
- * back on the reference code. Fortunately, the latter happens *very*
- * rarely.
- *
- * TODO Replace the inefficient block-by-block call to the assembler by a sweep
- * through the whole lot...
- */
-																							     											     
+   8 words are handled per iteration; first four words uses mm0-mm3,
+   last four words use mm4-mm7.
+*/
 static int quant_non_intra_mmx( struct QuantizerWorkSpace *wsp,
-								int16_t *src, int16_t *dst,
-								int q_scale_type,
-								int clipvalue,
-								int *nonsat_mquant)
+                                int16_t *src, int16_t *dst,
+                                int q_scale_type,
+                                int satlim,
+                                int *nonsat_mquant)
 {
+    int saturated;
+    int mquant = *nonsat_mquant;
+    int   coeff_count = 64*BLOCK_COUNT;
+    uint32_t nzflag, flags;
+    int16_t *psrc, *pdst, *pdiv, *pmul, *pdivbase, *pmulbase;
+    int i;
+    uint32_t tmp;
+    uint64_t negone_q,satlim_q;
 
-	int nzflag;
-	int flags = 0;
-	int saturated = 0;
-	int mquant = *nonsat_mquant;
-	uint16_t *quant_mat = wsp->inter_q_mat;
-	int comp;
-	uint16_t *i_quant_mat = wsp->i_inter_q_mat;
-	int imquant;
-	int16_t *psrc, *pdst;
+    /* Load -1 into negone_q */
+    pxor_r2r( mm6, mm6 );
+    pcmpeqw_r2r( mm6, mm6 );
+    movq_r2m( mm6, negone_q );
 
-	/* MMX routine does not work right for MQ=2 ... (no unsigned mult) */
-	if( mquant == 2 )
-	{
-		return quant_non_intra( wsp,
-								src, dst, q_scale_type, clipvalue, nonsat_mquant);
-	}
-	/* If available use the fast MMX quantiser.  It returns
-	   flags to signal if coefficients are outside its limited range or
-	   saturation would occur with the specified quantisation
-	   factor
-	   Top 16 bits - non zero quantised coefficient present
-	   Bits 8-15   - Saturation occurred
-	   Bits 0-7    - Coefficient out of range.
-	*/
+    /* Load satlim into satlim_q */
+    movd_g2r( satlim, mm7 );
+    punpcklwd_r2r( mm7, mm7 );
+    punpckldq_r2r( mm7, mm7 );
+    movq_r2m( mm7, satlim_q );
 
-	nzflag = 0;
-	pdst = dst;
-	psrc = src;
-	comp = 0; 
-	do
-	{
-		imquant = (IQUANT_SCALE/mquant);
-		flags = quantize_non_intra_mb_mmx( pdst, psrc, quant_mat, i_quant_mat, 
-								 imquant, mquant, clipvalue );
-		nzflag = (nzflag << 1) |( !!(flags & 0xffff0000));
-  
-		/* If we're saturating simply bump up quantization and start
-			from scratch...  if we can't avoid saturation by
-			quantising then we're hosed and we fall back to
-			saturation using the old C code.  */
-  
-		if( (flags & 0xff00) != 0 )
-		{
-			int new_mquant = next_larger_quant( q_scale_type, mquant );
-			if( new_mquant != mquant )
-			{
-				mquant = new_mquant;
-			}
-			else
-			{
-				saturated = 1;
-				break;
-			}
+    pdivbase = wsp->i_inter_q_tbl[mquant];
+    pmulbase = wsp->inter_q_tbl[mquant];
 
-			comp = 0; 
-			nzflag = 0;
-			pdst = dst;
-			psrc = src;
-		}
-		else
-		{
-			++comp;
-			pdst += 64;
-			psrc +=64;
-		}
-		/* Fall back to 32-bit(or better - if some hero(ine) made this work on
-			non 32-bit int machines ;-)) if out of dynamic range for MMX...
-		*/
-	}
-	while( comp < BLOCK_COUNT  && (flags & 0xff) == 0  );
+ restart:
+    flags = 0;
+    pdiv = pdivbase;
+    pmul = pmulbase;
+    saturated = 0;
+    nzflag = 0;
+    psrc = src;
+    pdst = dst;
+    for (i=0; i < coeff_count ; i+=8)
+    {
+        movq_m2r( psrc[0], mm3 ); // load values
+        movq_m2r( psrc[4], mm7 ); // load values
+        psllw_i2r( 4, mm3 );   // multiply by 16
+        psllw_i2r( 4, mm7 );   // multiply by 16
+        movq_r2r( mm3, mm0 );  // keep sign in mm3, make mm0=abs(mm3)
+        movq_r2r( mm7, mm4 );  // keep sign in mm3, make mm0=abs(mm3)
+        psraw_i2r( 15, mm3 );
+        psraw_i2r( 15, mm7 );
+        pxor_r2r( mm3, mm0 );
+        pxor_r2r( mm7, mm4 );
+        psubw_r2r( mm3, mm0 );
+        psubw_r2r( mm7, mm4 );
+        movq_m2r( pdiv[0], mm1 ); // "divide" by quant
+        movq_m2r( pdiv[4], mm5 ); // "divide" by quant
+        pmulhw_r2r( mm0, mm1 );
+        pmulhw_r2r( mm4, mm5 );
+        movq_m2r( pmul[0], mm2 ); // check that x*q > X-q; i.e. make sure x is not off by too much
+        movq_m2r( pmul[4], mm6 ); // check that x*q > X-q; i.e. make sure x is not off by too much
+        psubw_r2r( mm2, mm0 );
+        psubw_r2r( mm6, mm4 );
+        pmullw_r2r( mm1, mm2 );
+        pmullw_r2r( mm5, mm6 );
+        pcmpgtw_r2r( mm0, mm2 );
+        pcmpgtw_r2r( mm4, mm6 );
+        pxor_m2r( negone_q, mm2 );
+        pxor_m2r( negone_q, mm6 );
+        psubw_r2r( mm2, mm1 ); // if x*q <= X-q, increment x by 1
+        psubw_r2r( mm6, mm5 ); // if x*q <= X-q, increment x by 1
+        movq_r2r( mm1, mm0 ); // stash abs of result away
+        movq_r2r( mm5, mm4 ); // stash abs of result away
+        pxor_r2r( mm3, mm1 ); // make result have same sign as orig value
+        pxor_r2r( mm7, mm5 ); // make result have same sign as orig value
+        psubw_r2r( mm3, mm1 );        
+        psubw_r2r( mm7, mm5 );        
+        movq_r2m( mm1, pdst[0] ); // store result
+        movq_r2m( mm5, pdst[4] ); // store result
+        
+        por_r2r( mm5, mm1 );
+        movq_r2r( mm1, mm2 ); // set flags for non null responses
+        psrlq_i2r( 32, mm2 );
+        por_r2r( mm1, mm2 );
+        movd_r2g( mm2, tmp );
+        flags |= tmp;
 
+        pcmpgtw_m2r( satlim_q, mm0 ); // did result exceed satlim?
+        pcmpgtw_m2r( satlim_q, mm4 );
+        por_r2r( mm4, mm0 );
+        movq_r2r( mm0, mm1 );
+        psrlq_i2r( 32, mm1 );
+        por_r2r( mm1, mm0 );
+        movd_r2g( mm1, tmp );
+        saturated |= tmp;
 
-	/* Coefficient out of range or can't avoid saturation:
-	fall back to the original 32-bit int version: this is rare */
-	if(  (flags & 0xff) != 0 || saturated)
-	{
-		return quant_non_intra( wsp,
-								src, dst, q_scale_type, clipvalue, nonsat_mquant);
-	}
+        pdiv += 8;
+        pmul += 8;
+        pdst += 8;
+        psrc += 8;
 
-	*nonsat_mquant = mquant;
-	return nzflag;
+        if( (i & 63) == (63/8)*8 )
+        {
+            if( saturated )
+            {
+                int new_mquant = next_larger_quant( q_scale_type, mquant );
+                if( new_mquant != mquant )
+                {
+                    mquant = new_mquant;
+                    goto restart;
+                }
+                else
+                {
+                    return quant_non_intra(wsp, src, dst, 
+                                           q_scale_type,
+                                           satlim,
+                                           nonsat_mquant);
+                }
+            }
+
+            nzflag = (nzflag<<1) | !!flags;
+            flags = 0;
+            pdiv = pdivbase;
+            pmul = pmulbase;
+        }
+			
+    }
+    emms();
+
+    //nzflag = (nzflag<<1) | (!!flags);
+    return nzflag;
 }
 
 static void iquant_non_intra_m1_extmmx(struct QuantizerWorkSpace *wsp,
@@ -492,97 +387,137 @@ static int quant_weight_coeff_x86_inter( struct QuantizerWorkSpace *wsp,
 	return quant_weight_coeff_sum_mmx( blk, wsp->i_inter_q_mat );
 }
 
+#if 0
+static int quant_non_intra_test(struct QuantizerWorkSpace *wsp,
+                                int16_t *src, int16_t *dst,
+                                int q_scale_type,
+                                int satlim,
+                                int *nonsat_mquant)
+{
+    int16_t d2[64*BLOCK_COUNT];
+    int rv1,rv2,mq1,mq2;
 
-void init_x86_quantization( struct QuantizerCalls *qcalls, int mpeg1)
-    {
+    mq1=*nonsat_mquant;
+    rv1=quant_non_intra(wsp,src,d2,q_scale_type,satlim,&mq1);
+    rv2=quant_non_intra_mmx(wsp,src,dst,q_scale_type,satlim,nonsat_mquant);
+    mq2=*nonsat_mquant;
+
+    if( rv1!=rv2 )
+        mjpeg_warn("quant_non_intra disparity: ret=%d vs %d",rv1,rv2);
+    if( mq1!=mq2 )
+        mjpeg_warn("quant_non_intra disparity: mq=%d vs %d",mq1,mq2);
+    if( memcmp(d2,dst,64*BLOCK_COUNT*sizeof(int16_t)) ) {
+        int i;
+
+        mjpeg_warn("quant_non_intra disparity: dst differs");
+        for( i=0; i<64*BLOCK_COUNT; i++ ) {
+            if( d2[i]!=dst[i] )
+                mjpeg_warn("\t[%d]: %4d vs %4d",i,dst[i],d2[i]);
+        }
+    }
+
+    return rv2;
+}
+#endif
+
+static int quant_non_intra_can_use_mmx(struct QuantizerWorkSpace *wsp)
+{
+    int i;
+
+    for( i=0; i<64; i++ ) {
+        int q=wsp->inter_q_mat[i];
+
+        // if q<0, then bad things happen (mismatched sign)
+        // if q==0, then divide by 0 happens
+        // if q==1 or q==2, then 65536/q >= 32768 (negative sign when using signed multiply)
+        // if q>292, then q*112 (which is the max quant scale) >=32768 (again, negative sign when using signed multiply)
+        if( q<3 || q>292 )
+            return 0;
+    }
+    return 1;
+}
+
+void init_x86_quantization( struct QuantizerCalls *qcalls,
+                            struct QuantizerWorkSpace *wsp,
+                            int mpeg1)
+{
     int flags = cpu_accel();
     int d_quant_nonintra, d_weight_intra, d_weight_nonintra, d_iquant_intra;
     int d_iquant_nonintra;
     const char *opt_type1 = "", *opt_type2 = "";
 
     if  ((flags & ACCEL_X86_MMX) != 0 ) /* MMX CPU */
-	{
+    {
 	d_quant_nonintra = disable_simd("quant_nonintra");
 	d_weight_intra = disable_simd("quant_weight_intra");
 	d_weight_nonintra = disable_simd("quant_weight_nonintra");
 	d_iquant_intra = disable_simd("iquant_intra");
 	d_iquant_nonintra = disable_simd("iquant_nonintra");
 
-	if ((flags & ACCEL_X86_3DNOW) != 0)
-	   {
-	   if  (d_quant_nonintra == 0)
-	       {
-	       opt_type1 = "3DNOW and";
-	       qcalls->pquant_non_intra = quant_non_intra_3dnow;
-	       }
-	   }
-        else if ((flags & ACCEL_X86_SSE) != 0)
-	    {
-	    if  (d_quant_nonintra == 0)
-		{
-	        opt_type1 = "SSE and";
-	        qcalls->pquant_non_intra = quant_non_intra_sse;
-		}
-	    }
-	else 
-	    {
-	    if  (d_quant_nonintra == 0)
-		{
+        if  (d_quant_nonintra == 0)
+        {
+            if ((flags & ACCEL_X86_3DNOW) != 0)
+            {
+                opt_type1 = "3DNOW and";
+                qcalls->pquant_non_intra = quant_non_intra_3dnow;
+            }
+            else if( quant_non_intra_can_use_mmx(wsp) )
+            {
 	        opt_type1 = "MMX and";
 	        qcalls->pquant_non_intra = quant_non_intra_mmx;
-		}
-	    }
+            }
+        }
 
 	if ((flags & ACCEL_X86_MMXEXT) != 0)
-	    {
+        {
 	    opt_type2 = "EXTENDED MMX";
 	    if (d_weight_intra == 0)
-	       qcalls->pquant_weight_coeff_intra = quant_weight_coeff_x86_intra;
+                qcalls->pquant_weight_coeff_intra = quant_weight_coeff_x86_intra;
 	    if (d_weight_nonintra == 0)
-	       qcalls->pquant_weight_coeff_inter = quant_weight_coeff_x86_inter;
+                qcalls->pquant_weight_coeff_inter = quant_weight_coeff_x86_inter;
 
             if (mpeg1)
-		{
+            {
 		if (d_iquant_nonintra == 0)
-                   qcalls->piquant_non_intra = iquant_non_intra_m1_extmmx;
-		}
-             else
-		{
+                    qcalls->piquant_non_intra = iquant_non_intra_m1_extmmx;
+            }
+            else
+            {
 		if (d_iquant_nonintra == 0)
-                   qcalls->piquant_non_intra = iquant_non_intra_m2_mmx;
-		}
-             }
-	 else
-	     {
-	     opt_type2 = "MMX";
-	     if (d_weight_intra == 0)
+                    qcalls->piquant_non_intra = iquant_non_intra_m2_mmx;
+            }
+        }
+        else
+        {
+            opt_type2 = "MMX";
+            if (d_weight_intra == 0)
 	        qcalls->pquant_weight_coeff_intra = quant_weight_coeff_x86_intra;
-	     if (d_weight_nonintra == 0)
+            if (d_weight_nonintra == 0)
 	        qcalls->pquant_weight_coeff_inter = quant_weight_coeff_x86_inter;
 
-             if (mpeg1)
-		{
+            if (mpeg1)
+            {
 		if (d_iquant_nonintra == 0)
-                   qcalls->piquant_non_intra = iquant_non_intra_m1_mmx;
-		}
-             else
-		{
+                    qcalls->piquant_non_intra = iquant_non_intra_m1_mmx;
+            }
+            else
+            {
 		if (d_iquant_nonintra == 0)
-                   qcalls->piquant_non_intra = iquant_non_intra_m2_mmx;
-		}
-	     }
+                    qcalls->piquant_non_intra = iquant_non_intra_m2_mmx;
+            }
+        }
 
-	 if  (d_quant_nonintra)
-	     mjpeg_info(" Disabling quant_non_intra");
-	 if  (d_iquant_intra)
-	     mjpeg_info(" Disabling iquant_intra");
-	 if  (d_iquant_nonintra)
-	     mjpeg_info(" Disabling iquant_nonintra");
-	 if  (d_weight_intra)
-	     mjpeg_info(" Disabling quant_weight_intra");
-	 if (d_weight_nonintra)
-	     mjpeg_info(" Disabling quant_weight_nonintra");
+        if  (d_quant_nonintra)
+            mjpeg_info(" Disabling quant_non_intra");
+        if  (d_iquant_intra)
+            mjpeg_info(" Disabling iquant_intra");
+        if  (d_iquant_nonintra)
+            mjpeg_info(" Disabling iquant_nonintra");
+        if  (d_weight_intra)
+            mjpeg_info(" Disabling quant_weight_intra");
+        if (d_weight_nonintra)
+            mjpeg_info(" Disabling quant_weight_nonintra");
 
 	mjpeg_info( "SETTING %s %s for QUANTIZER!", opt_type1, opt_type2);
-	}
     }
+}
