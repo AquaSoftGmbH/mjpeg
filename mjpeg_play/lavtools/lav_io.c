b77372e7e97219e789fffc123aa2b7c8d1d6be14
sms00 <>
2005-11-22 05:32:37 +0000

    Ugly hack time.  In the CVS version of libquicktime the quicktime_t structure has become an opaque data type and the underlying filedescriptor must be obtained by using the accessor function lqt_fileno instead of the ".streamer" member (which is no longer available).  However, libquicktime 0.9.8 has not been released yet.   IF libquicktime is being used then test for lqt_fileno() being present and define HAVE_LQT_FILENO for use in 1 place.
    When libquicktime has a release this can all go away when the required version
    of libquicktime is bumped to 0.9.8

--- mjpeg_play/lavtools/lav_io.c
+++ mjpeg_play/lavtools/lav_io.c
@@ -1395,8 +1395,12 @@ int lav_fileno(lav_file_t *lav_file)
          break;
 #ifdef HAVE_LIBQUICKTIME
       case 'q':
-         res = fileno(((quicktime_t *)lav_file->qt_fd)->stream);
-         break;
+#ifdef HAVE_LQT_FILENO
+	res = lqt_fileno((quicktime_t *)lav_file->qt_fd);
+#else
+	res = fileno(((quicktime_t *)lav_file->qt_fd)->stream);
+#endif
+	break;
 #endif
       default:
          res = -1;
