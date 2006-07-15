5e7dd6de63ed3d153107ff2f75f0e97977de6eac
sms00 <>
2006-07-15 08:07:19 +0000

    Fix the usage() summary and coincident comments.  The proper usage is -v -v and not -vs to increase the debug/verbosity level.

--- mjpeg_play/lavtools/y4mstabilizer.c
+++ mjpeg_play/lavtools/y4mstabilizer.c
@@ -10,7 +10,7 @@
  *
  * Usage: y4mstabilizer [-v] [-a <alpha>] [-r <srchRadius>]
  *
- *	-v		Verbose.   Move -vs make it more verbose.
+ *	-v		Verbose.  Add additional -v to increase verbosity/debug
  *	-a <alpha>	The alpha value is a "viscosity" measure (see below).
  *	-r <srchRadius>	How far to look for movement.
  *	-s <stride>	How far apart the motion search points are.
@@ -308,7 +308,7 @@ usage (void)
 "\n"
 "Usage: y4mstabilizer [-v] [-a <alpha>] [-r <srchRadius>]\n"
 "\n"
-"	-v		Verbose.   Move -vs make it more verbose.\n"
+"	-v		Verbose.  Repeat -v to increase verbosity/debug info\n"
 "	-a <alpha>	A \"viscosity\" measure (see below).\n"
 "	-r <srchRadius>	How far to look for movement.\n"
 "	-s <stride>	How far apart the motion search points are.\n"
