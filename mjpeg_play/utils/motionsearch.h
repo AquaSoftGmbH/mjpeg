a12ef5ac66f5500cb4d4e3d48ef8f60ad0305da6
sms00 <>
2007-05-15 07:04:19 +0000

    Code hygiene and preparation for promotion to an installed header file.

--- mjpeg_play/utils/motionsearch.h
+++ mjpeg_play/utils/motionsearch.h
@@ -16,11 +16,11 @@
  * 02111-1307, USA.
  *
  */
-
+
 #ifndef __MOTIONSEARCH_H__
 #define __MOTIONSEARCH_H__
 
-#include "mjpeg_types.h"
+#include <mjpeg_types.h>
 
 struct me_result
 {
@@ -40,55 +40,42 @@ typedef struct _me_result_vec {
 
 /*
  * Function pointers for selecting CPU specific implementations
- *
  * Top-level motion estimation entry points.
- *
- */
+*/
 
 /*
  * Use given set of 2*2 sub-sampled motion estimates generated using
  * pbuild_sub22_mests to generate a best 1-pel motion estimate.
- *
- */
+*/
 
-extern void (*pfind_best_one_pel)( me_result_set *sub22set,
-								   uint8_t *org, uint8_t *blk,
-								   int i0, int j0,
-								   int ihigh, int jhigh,
-								   int rowstride, int h, 
-								   me_result_s *res
-	);
+extern void (*pfind_best_one_pel)(me_result_set *sub22set,
+				  uint8_t *org, uint8_t *blk,
+				  int i0, int j0, int ihigh, int jhigh,
+				  int rowstride, int h, me_result_s *res);
 
 /*
  * Use given set of 4*4 sub-sampled motion estimates generated using
- * pbuild_sub44_mests to generate a set of best 2*2 sub-sampled motion
- * estimates.
- */
+ * pbuild_sub44_mests to generate a set of best 2*2 sub-sampled motion estimates
+*/
 
-extern int (*pbuild_sub22_mests)( me_result_set *sub44set,
-								  me_result_set *sub22set,
-								  int i0,  int j0, int ihigh, int jhigh, 
-								  int null_mc_sad,
-								  uint8_t *s22org,  uint8_t *s22blk, 
-								  int frowstride, int fh,
-								  int reduction
-	);
+extern int (*pbuild_sub22_mests)(me_result_set *sub44set,
+				 me_result_set *sub22set,
+				 int i0,  int j0, int ihigh, int jhigh, 
+				 int null_mc_sad,
+				 uint8_t *s22org,  uint8_t *s22blk, 
+				 int frowstride, int fh, int reduction);
 
 /*
  * Generate a set the best 4*4 sub-sampled motion estimates
  * from the specifed rectangle of candidates in s44org with respect
  * to the reference block s44blk
- */
+*/
 
-
-extern int (*pbuild_sub44_mests)( me_result_set *sub44set,
-								  int ilow, int jlow, int ihigh, int jhigh, 
-								  int i0, int j0,
-								  int null_mc_sad,
-								  uint8_t *s44org, uint8_t *s44blk, 
-								  int qrowstride, int qh,
-								  int reduction );
-
+extern int (*pbuild_sub44_mests)(me_result_set *sub44set,
+				 int ilow, int jlow, int ihigh, int jhigh, 
+				 int i0, int j0, int null_mc_sad,
+				 uint8_t *s44org, uint8_t *s44blk, 
+				 int qrowstride, int qh, int reduction);
 
 /*
  * Lower level difference comparison routines
@@ -104,49 +91,43 @@ extern int (*pbuild_sub44_mests)( me_result_set *sub44set,
  * _xy suffix:  Difference with half-pel sub-sampling offset.
  *              (i.e. _00 is just ordinary no sub-sampling)
  * mests - Motion estimate results relative motion vector with sad.
- */
+*/
 
-extern int (*psumsq_sub22)( uint8_t *blk1, uint8_t *blk2,
-						 int rowstride, int h);
-extern int (*pbsumsq_sub22)( uint8_t *blk1f, uint8_t *blk1b, 
-						  uint8_t *blk2,
-						  int rowstride, int h);
+extern int (*psumsq_sub22)(uint8_t *blk1, uint8_t *blk2, int rowstride, int h);
+extern int (*pbsumsq_sub22)(uint8_t *blk1f, uint8_t *blk1b, 
+				uint8_t *blk2, int rowstride, int h);
 
 extern void (*pvariance)(uint8_t *mb, int size, int rowstride,
-						 uint32_t *p_variance, uint32_t *p_mean);
+				uint32_t *p_variance, uint32_t *p_mean);
 
+extern int (*psad_sub22)(uint8_t *blk1, uint8_t *blk2,  int frowstride, int fh);
+extern int (*psad_sub44)(uint8_t *blk1, uint8_t *blk2,  int qrowstride, int qh);
+extern int (*psad_00)(uint8_t *blk1, uint8_t *blk2,  int rowstride, int h, int distlim);
+extern int (*psad_01)(uint8_t *blk1, uint8_t *blk2, int rowstride, int h);
+extern int (*psad_10)(uint8_t *blk1, uint8_t *blk2, int rowstride, int h);
+extern int (*psad_11)(uint8_t *blk1, uint8_t *blk2, int rowstride, int h);
 
-extern int (*psad_sub22) ( uint8_t *blk1, uint8_t *blk2,  int frowstride, int fh);
-extern int (*psad_sub44) ( uint8_t *blk1, uint8_t *blk2,  int qrowstride, int qh);
-extern int (*psad_00) ( uint8_t *blk1, uint8_t *blk2,  int rowstride, int h, int distlim);
-extern int (*psad_01) (uint8_t *blk1, uint8_t *blk2, int rowstride, int h);
-extern int (*psad_10) (uint8_t *blk1, uint8_t *blk2, int rowstride, int h);
-extern int (*psad_11) (uint8_t *blk1, uint8_t *blk2, int rowstride, int h);
-
-extern int (*psumsq) (uint8_t *blk1, uint8_t *blk2,
-					  int rowstride, int hx, int hy, int h);
+extern int (*psumsq)(uint8_t *blk1, uint8_t *blk2,
+			int rowstride, int hx, int hy, int h);
   
   
-extern int (*pbsumsq) (uint8_t *pf, uint8_t *pb,
-					   uint8_t *p2, int rowstride, int hxf, int hyf, int hxb, int hyb, int h);
+extern int (*pbsumsq) (uint8_t *pf, uint8_t *pb, uint8_t *p2, int rowstride, 
+			int hxf, int hyf, int hxb, int hyb, int h);
 
-extern int (*pbsad) (uint8_t *pf, uint8_t *pb,
-					 uint8_t *p2, int rowstride, int hxf, int hyf, int hxb, int hyb, int h);
+extern int (*pbsad) (uint8_t *pf, uint8_t *pb, uint8_t *p2, int rowstride, 
+			int hxf, int hyf, int hxb, int hyb, int h);
 
-extern void (*psubsample_image) ( uint8_t *image, int rowstride, 
-					  uint8_t *sub22_image, 
-					  uint8_t *sub44_image);
+extern void (*psubsample_image) (uint8_t *image, int rowstride, 
+				  uint8_t *sub22_image, uint8_t *sub44_image);
 
 #ifdef  __cplusplus
 extern "C" {
 #endif
 
-void init_motion_search(void);
-void reset_motion_simd(char *);
+void init_motion_search(void), reset_motion_simd(char *);
 int round_search_radius( int radius );
 
 #ifdef  __cplusplus
 }
 #endif
-
 #endif  /* __MOTIONSEARCH_H__ */
